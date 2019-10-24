1226ad8438acbdd6714dee4b55f47824313174b2
Nico <itsarabbit@gmail.com>
2019-10-24 20:22:16 +0200

    remove stupid bad generation code

--- Game/Weapons/MeleeWeapon.cpp
+++ Game/Weapons/MeleeWeapon.cpp
@@ -14,11 +14,6 @@ MeleeWeapon::MeleeWeapon(MeleeWeapon&& aOther) noexcept
 	aOther.myBaseWeaponSwingRate = 1.0f;
 }
 
-MeleeWeapon::MeleeWeapon(const SPower aPower)
-{
-	Generate(aPower);
-}
-
 MeleeWeapon::MeleeWeapon()
 {
 
@@ -36,87 +31,7 @@ void MeleeWeapon::Inspect()
 	{
 		property->Inspect();
 	}
-	const f32 currentPower = GetPower();
-	ImGui::Text("Current estimated power = %f", currentPower);
-
-	static f32 Power = 20.f;
-	
-	if (ImGui::Button("Strengthen"))
-	{
-		Strengthen(Power);
-	}
-
-	ImGui::SameLine();
-
-	if (ImGui::Button("Weaken"))
-	{
-		Weaken(Power);
-	}
-
-	ImGui::SliderFloat("Power to use when Strengthening/Weakening", &Power, 0.f, 1000.f, "%.3f", 2.0f);
 
 	ImGui::Separator();
 }
 
-void MeleeWeapon::Strengthen(const SPower aPower)
-{
-	switch (SystemPtr<Randomizer>()->GetRandomNumber(0, 6))
-	{
-	case 0:
-	case 1:
-		IncreaseDamage(aPower);
-		break;
-	case 2:
-	case 3:
-		IncreaseAttackSpeed(aPower);
-		break;
-	case 4:
-		Strengthen(aPower / 2.f);
-		Strengthen(aPower / 2.f);
-		break;
-	case 5:
-		Weaken(aPower);
-		Strengthen(aPower);
-		Strengthen(aPower);
-		break;
-	default:
-		WmLog("Should not be reached; MeleeWeapon::Strengthen");
-	}
-}
-
-void MeleeWeapon::IncreaseDamage(const SPower aPower)
-{
-	myBaseWeaponDamage += aPower / myBaseWeaponSwingRate;
-
-	if (myBaseWeaponDamage < 1.0f)
-	{
-		myBaseWeaponDamage = 1.f;
-	}
-}
-
-void MeleeWeapon::IncreaseAttackSpeed(const SPower aPower)
-{
-	const f32 baseDamage = 50.f;
-	myBaseWeaponSwingRate += (aPower / baseDamage) / (myBaseWeaponDamage / baseDamage);
-
-	const f32 MinimumSwingRate = 0.15f;
-	if (myBaseWeaponSwingRate < MinimumSwingRate)
-		myBaseWeaponSwingRate = MinimumSwingRate;
-}
-
-SPower MeleeWeapon::GetPower() const
-{
-	return myBaseWeaponDamage * myBaseWeaponSwingRate;
-}
-
-void MeleeWeapon::Generate(const SPower aPower)
-{
-	const SPower half = aPower / 2.f;
-
-	IncreaseDamage(half);
-
-	Strengthen(half);
-
-	for (i32 i = 0, e = SystemPtr<Randomizer>()->GetRandomNumber(1, 4); i < e; i++)
-		mySwings.emplace_back();
-}
--- Game/Weapons/MeleeWeapon.h
+++ Game/Weapons/MeleeWeapon.h
@@ -1,21 +1,15 @@
 #pragma once
 #include "Weapons/WeaponProperty/WeaponProperty.h"
-#include "Generation/GeneratedObject.h"
-#include "Generation/GenerationParameters/Power.h"
 #include "Weapons/WeaponSwing.h"
 
 class MeleeWeapon
 	: public NonCopyable
-	, public GeneratedObject<SPower>
 {
 public:
 	MeleeWeapon();
-	MeleeWeapon(const SPower aPower);
 	MeleeWeapon(MeleeWeapon&& aOther) noexcept;
 
 	void Inspect();
-
-	[[nodiscard]] virtual SPower GetPower() const override;
 
 	std::vector<SSwing> mySwings;
 	
@@ -23,12 +17,6 @@ public:
 	f32 myBaseWeaponSwingRate = 1.f;
 
 private:
-	virtual void Strengthen(const SPower aPower) override;
-	void IncreaseDamage(const SPower aPower);
-	void IncreaseAttackSpeed(const SPower aPower);
-
 	std::vector<std::unique_ptr<WeaponProperty>> myProperties;
-
-	virtual void Generate(const SPower aPower) override;
 };
 

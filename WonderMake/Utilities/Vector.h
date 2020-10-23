d6ecb63600a18070b1f59e4451f04fe510518a18
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2020-10-23 22:33:41 +0200

    [Refactor] Combined all SVector definitions and declarations.

--- WonderMake/Utilities/Vector.h
+++ WonderMake/Utilities/Vector.h
@@ -59,7 +59,8 @@ public:
 };
 #pragma warning(pop)
 
-template <typename TRep, u32 TSize> requires (TSize > 4)
+template <typename TRep, u32 TSize>
+requires (TSize > 4)
 struct SVectorBase<TRep, TSize>
 {
 public:
@@ -75,14 +76,18 @@ struct SVector
 
 	constexpr SVector() = default;
 
-	template<typename... Reps>
-	using TVectorMembers = typename std::enable_if_t<sizeof...(Reps) == TSize && std::conjunction_v<std::is_convertible<Reps, TRep>...>>;
+	template<typename ...TArgs>
+	constexpr SVector(TArgs... aArgs) noexcept
+		requires (sizeof...(TArgs) == TSize && std::conjunction_v<std::is_convertible<TArgs, TRep>...>)
+	{
+		u32 i = 0;
 
-	template<typename ...TArgs, typename = TVectorMembers<TArgs...>>
-	constexpr SVector(TArgs... aArgs) noexcept;
+		(((*this)[i++] = static_cast<TRep>(aArgs)), ...);
+	}
 	
 	template<typename TRotation>
-	SVector<TRep, TSize>& Rotate(const TRotation aRotation) noexcept requires (TSize == 2 && std::is_floating_point_v<typename TRotation::Representation>)
+	SVector<TRep, TSize>& Rotate(const TRotation aRotation) noexcept
+		requires (TSize == 2 && std::is_floating_point_v<typename TRotation::Representation>)
 	{
 		using RotationRep = SRadian<typename TRotation::Representation>;
 
@@ -97,18 +102,21 @@ struct SVector
 
 		return (*this);
 	}
-	template<typename TRotation = SRadian<TRep>> requires (TSize == 2 && std::is_floating_point_v<typename TRotation::Representation>)
+	template<typename TRotation = SRadian<TRep>>
 	[[nodiscard]] TRotation GetRotation() const noexcept
+		requires (TSize == 2 && std::is_floating_point_v<typename TRotation::Representation>)
 	{
 		return MathUtility::Atan2<TRotation>(SVectorBase<TRep, Size>::X, SVectorBase<TRep, Size>::Y);
 	}
 
 	// Lowers or raises the dimension of the vector by one
-	[[nodiscard]] constexpr TRep Demote() const noexcept requires (TSize == 2)
+	[[nodiscard]] constexpr TRep Demote() const noexcept
+		requires (TSize == 2)
 	{
 		return (*this)[0];
 	}
-	[[nodiscard]] constexpr SVector<TRep, TSize - 1> Demote() const noexcept requires (TSize > 2)
+	[[nodiscard]] constexpr SVector<TRep, TSize - 1> Demote() const noexcept
+		requires (TSize > 2)
 	{
 		SVector<TRep, Size - 1> ReturnVal;
 
@@ -119,47 +127,185 @@ struct SVector
 
 		return ReturnVal;
 	}
-	[[nodiscard]] constexpr SVector<TRep, TSize + 1> Promote(const TRep LastValue = {}) const noexcept;
+	[[nodiscard]] constexpr SVector<TRep, TSize + 1> Promote(const TRep LastValue = {}) const noexcept
+	{
+		SVector<TRep, TSize + 1> ReturnVal;
 
-	[[nodiscard]] constexpr TRep& operator[] (const u32 Index) noexcept;
-	[[nodiscard]] constexpr const TRep& operator[] (const u32 Index) const noexcept;
+		for (u32 u = 0; u < TSize; u++)
+		{
+			ReturnVal[u] = (*this)[u];
+		}
 
-	[[nodiscard]] constexpr char GetMemberName(const u32 Index) const noexcept;
+		ReturnVal[TSize] = LastValue;
 
-	[[nodiscard]] constexpr SVector<TRep, TSize> operator-() const noexcept;
+		return ReturnVal;
+	}
 
-	constexpr SVector<TRep, TSize>& operator*=(const TRep aMultiplier) noexcept;
-	constexpr SVector<TRep, TSize>& operator/=(const TRep aDivisor) noexcept;
-	constexpr SVector<TRep, TSize>& operator+=(const SVector<TRep, TSize>& aRightVector) noexcept;
-	constexpr SVector<TRep, TSize>& operator-=(const SVector<TRep, TSize>& aRightVector) noexcept;
+	[[nodiscard]] constexpr TRep& operator[] (const u32 Index) noexcept
+	{
+		assert(Index < TSize);
+		return this->MemberArray[Index];
+	}
+	[[nodiscard]] constexpr const TRep& operator[] (const u32 Index) const noexcept
+	{
+		assert(Index < TSize);
+		return this->MemberArray[Index];
+	}
 
-	constexpr bool operator==(const SVector<TRep, TSize>& aRightVector) const noexcept;
-	constexpr bool operator!=(const SVector<TRep, TSize>& aRightVector) const noexcept;
+	[[nodiscard]] constexpr char GetMemberName(const u32 Index) const noexcept
+	{
+		switch (Index)
+		{
+		case 0:
+			return 'X';
+		case 1:
+			return 'Y';
+		case 2:
+			return 'Z';
+		case 3:
+			return 'W';
+		default:
+			return '1' + char(Index);
+		}
+	}
 
-	[[nodiscard]] constexpr TRep LengthSquared() const noexcept;
-	[[nodiscard]] constexpr TRep Length() const noexcept;
-	[[nodiscard]] constexpr TRep DistanceTo(const SVector<TRep, TSize> aOther) const noexcept;
+	[[nodiscard]] constexpr SVector<TRep, TSize> operator-() const noexcept
+	{
+		SVector<TRep, TSize> ReturnVal;
 
-	[[nodiscard]] constexpr TRep Dot(const SVector<TRep, TSize> aOther) const noexcept;
+		for (u32 u = 0; u < TSize; u++)
+		{
+			ReturnVal[u] = -((*this)[u]);
+		}
 
-	constexpr void Normalize() noexcept;
-	[[nodiscard]] constexpr SVector<TRep, TSize> GetNormalized() const noexcept;
+		return ReturnVal;
+	}
+
+	constexpr SVector<TRep, TSize>& operator*=(const TRep aRhs) noexcept
+	{
+		*this = *this * aRhs;
+
+		return *this;
+	}
+	constexpr SVector<TRep, TSize>& operator/=(const TRep aRhs) noexcept
+	{
+		*this = *this / aRhs;
+
+		return *this;
+	}
+	constexpr SVector<TRep, TSize>& operator+=(const SVector<TRep, TSize>& aRhs) noexcept
+	{
+		*this = *this + aRhs;
+
+		return *this;
+	}
+	constexpr SVector<TRep, TSize>& operator-=(const SVector<TRep, TSize>& aRhs) noexcept
+	{
+		*this = *this - aRhs;
+
+		return *this;
+	}
+
+	constexpr bool operator==(const SVector<TRep, TSize>& aRhs) const noexcept
+	{
+		for (u32 u = 0; u < TSize; u++)
+		{
+			if ((*this)[u] != aRhs[u])
+				return false;
+		}
+
+		return true;
+	}
+	constexpr bool operator!=(const SVector<TRep, TSize>& aRhs) const noexcept
+	{
+		return !(*this == aRhs);
+	}
+
+	[[nodiscard]] constexpr TRep LengthSquared() const noexcept
+	{
+		TRep total = 0;
+
+		for (u32 u = 0; u < TSize; u++)
+		{
+			total += (*this)[u] * (*this)[u];
+		}
+
+		return total;
+	}
+	[[nodiscard]] constexpr TRep Length() const noexcept
+	{
+		return static_cast<TRep>(std::sqrt(LengthSquared()));
+	}
+	[[nodiscard]] constexpr TRep DistanceTo(const SVector<TRep, TSize> aRhs) const noexcept
+	{
+		return (aRhs - *this).Length();
+	}
+
+	[[nodiscard]] constexpr TRep Dot(const SVector<TRep, TSize> aRhs) const noexcept
+	{
+		TRep sum = 0;
+		for (u32 i = 0; i < TSize; i++)
+		{
+			sum += (*this)[i] * aRhs[i];
+		}
+
+		return sum;
+	}
+
+	constexpr void Normalize() noexcept
+	{
+		const TRep length = Length();
+
+		if (length == 0)
+			return;
+
+		for (u32 u = 0; u < TSize; u++)
+		{
+			(*this)[u] /= length;
+		}
+	}
+	[[nodiscard]] constexpr SVector<TRep, TSize> GetNormalized() const noexcept
+	{
+		SVector<TRep, TSize> retVec = *this;
+
+		retVec.Normalize();
+
+		return retVec;
+	}
 	
-	[[nodiscard]] constexpr SVector<TRep, TSize> GetPerpendicularCounterClockWise() const noexcept requires (TSize == 2)
+	[[nodiscard]] constexpr SVector<TRep, TSize> GetPerpendicularCounterClockWise() const noexcept
+		requires (TSize == 2)
 	{
 		return SVector<TRep, TSize>(-(*this).Y, (*this).X);
 	}
-	[[nodiscard]] constexpr SVector<TRep, TSize> GetPerpendicularClockWise() const noexcept requires (TSize == 2)
+	[[nodiscard]] constexpr SVector<TRep, TSize> GetPerpendicularClockWise() const noexcept
+		requires (TSize == 2)
 	{
 		return SVector<TRep, TSize>((*this).Y, -(*this).X);
 	}
-	[[nodiscard]] constexpr SVector<TRep, TSize> GetNormal() const noexcept requires (TSize == 2)
+	[[nodiscard]] constexpr SVector<TRep, TSize> GetNormal() const noexcept
+		requires (TSize == 2)
 	{
 		return GetPerpendicularClockWise();
 	}
 
-	[[nodiscard]] constexpr static SVector<TRep, TSize> Zero() noexcept;
-	[[nodiscard]] constexpr static SVector<TRep, TSize> One() noexcept;
+	[[nodiscard]] constexpr static SVector<TRep, TSize> Zero() noexcept
+	{
+		SVector<TRep, TSize> retVec;
+
+		return retVec;
+	}
+	[[nodiscard]] constexpr static SVector<TRep, TSize> One() noexcept
+	{
+		SVector<TRep, TSize> retVec;
+
+		for (u32 u = 0; u < TSize; u++)
+		{
+			retVec[u] = 1;
+		}
+
+		return retVec;
+	}
 };
 
 template <typename TRep>
@@ -181,15 +327,56 @@ using SVector2u = SVector2<u32>;
 using SVector3u = SVector3<u32>;
 using SVector4u = SVector4<u32>;
 
-template <typename TRep, u32 TSize>
-[[nodiscard]] constexpr static SVector<TRep, TSize> operator-(const SVector<TRep, TSize>& aLeftVector, const SVector<TRep, TSize>& aRightVector) noexcept;
-template <typename TRep, u32 TSize>
-[[nodiscard]] constexpr static SVector<TRep, TSize> operator+(const SVector<TRep, TSize>& aLeftVector, const SVector<TRep, TSize>& aRightVector) noexcept;
-template <typename TRep, u32 TSize>
-[[nodiscard]] constexpr static SVector<TRep, TSize> operator*(const TRep aMultiplier, const SVector<TRep, TSize>& aLeftVector) noexcept;
-template <typename TRep, u32 TSize>
-[[nodiscard]] constexpr static SVector<TRep, TSize> operator*(const SVector<TRep, TSize>& aLeftVector, const TRep aMultiplier) noexcept;
-template <typename TRep, u32 TSize>
-[[nodiscard]] constexpr static SVector<TRep, TSize> operator/(const SVector<TRep, TSize>& aLeftVector, const TRep aDivisor) noexcept;
+template<typename TRep, u32 TSize>
+[[nodiscard]] constexpr static SVector<TRep, TSize> operator-(const SVector<TRep, TSize>& aLhs, const SVector<TRep, TSize>& aRhs) noexcept
+{
+	SVector<TRep, TSize> retVec = aLhs;
 
-#include "Vector.inl"
+	for (u32 u = 0; u < TSize; u++)
+	{
+		retVec[u] -= aRhs[u];
+	}
+
+	return retVec;
+}
+template<typename TRep, u32 TSize>
+[[nodiscard]] constexpr static SVector<TRep, TSize> operator+(const SVector<TRep, TSize>& aLhs, const SVector<TRep, TSize>& aRhs) noexcept
+{
+	SVector<TRep, TSize> retVec = aLhs;
+
+	for (u32 u = 0; u < TSize; u++)
+	{
+		retVec[u] += aRhs[u];
+	}
+
+	return retVec;
+}
+template<typename TRep, u32 TSize>
+[[nodiscard]] constexpr static SVector<TRep, TSize> operator*(const TRep aLhs, const SVector<TRep, TSize>& aRhs) noexcept
+{
+	return aRhs * aLhs;
+}
+template<typename TRep, u32 TSize>
+[[nodiscard]] constexpr static SVector<TRep, TSize> operator*(const SVector<TRep, TSize>& aLhs, const TRep aRhs) noexcept
+{
+	SVector<TRep, TSize> retVec = aLhs;
+
+	for (u32 u = 0; u < TSize; u++)
+	{
+		retVec[u] *= aRhs;
+	}
+
+	return retVec;
+}
+template<typename TRep, u32 TSize>
+[[nodiscard]] constexpr static SVector<TRep, TSize> operator/(const SVector<TRep, TSize>& aLhs, const TRep aRhs) noexcept
+{
+	SVector<TRep, TSize> retVec = aLhs;
+
+	for (u32 u = 0; u < TSize; u++)
+	{
+		retVec[u] /= aRhs;
+	}
+
+	return retVec;
+}

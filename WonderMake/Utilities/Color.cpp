f1639dfaabe33059b1c0b452d78f9fbb00a6b791
Nicos <nicolas.gustafsson@gmail.com>
2021-08-23 14:14:15 +0200

    HSV support for colors

--- WonderMake/Utilities/Color.cpp
+++ WonderMake/Utilities/Color.cpp
@@ -1,2 +1,107 @@
 #include "pch.h"
 #include "Color.h"
+
+template <>
+SColorTemplate<EColorSpace::HSV> ConvertColor(SColorTemplate<EColorSpace::LinearRGB> aFrom)
+{
+    const f32 minColorValue = WmMath::Min(WmMath::Min(aFrom.R, aFrom.G), aFrom.B);
+    const f32 maxColorValue = WmMath::Max(WmMath::Max(aFrom.R, aFrom.G), aFrom.B);
+    const f32 colorValueDelta = maxColorValue - minColorValue;
+
+    SColorHSV hsv;
+
+    if (colorValueDelta > 0) {
+        if (maxColorValue == aFrom.R) {
+            hsv.Hue = 60 * (WmMath::Mod(((aFrom.G - aFrom.B) / colorValueDelta), 6.f));
+        }
+        else if (maxColorValue == aFrom.G) {
+            hsv.Hue = 60 * (((aFrom.B - aFrom.R) / colorValueDelta) + 2);
+        }
+        else if (maxColorValue == aFrom.B) {
+            hsv.Hue = 60 * (((aFrom.R - aFrom.G) / colorValueDelta) + 4);
+        }
+
+        if (maxColorValue > 0) {
+            hsv.Saturation = colorValueDelta / maxColorValue;
+        }
+        else {
+            hsv.Saturation = 0;
+        }
+
+        hsv.Value = maxColorValue;
+    }
+    else {
+        hsv.Hue = 0;
+        hsv.Saturation = 0;
+        hsv.Value = maxColorValue;
+    }
+
+    if (hsv.Hue < 0) {
+        hsv.Hue = SDegreeF32(360.f) + hsv.Hue;
+    }
+
+	return hsv;
+}
+
+template <>
+SColorTemplate<EColorSpace::LinearRGB> ConvertColor(SColorTemplate<EColorSpace::HSV> aFrom)
+{
+    SColor rgb;
+
+    f32 chroma = aFrom.Value * aFrom.Saturation;
+    f32 hueInSixth = WmMath::Mod(aFrom.Hue.Rotation / 60.0f, 6.f);
+    f32 fX = chroma * (1 - fabs(WmMath::Mod(hueInSixth, 2.f) - 1.f));
+    f32 baseValue = aFrom.Value - chroma;
+
+    if (0 <= hueInSixth && hueInSixth < 1) {
+        rgb.R = chroma;
+        rgb.G = fX;
+        rgb.B = 0;
+    }
+    else if (1 <= hueInSixth && hueInSixth < 2) {
+        rgb.R = fX;
+        rgb.G = chroma;
+        rgb.B = 0;
+    }
+    else if (2 <= hueInSixth && hueInSixth < 3) {
+        rgb.R = 0;
+        rgb.G = chroma;
+        rgb.B = fX;
+    }
+    else if (3 <= hueInSixth && hueInSixth < 4) {
+        rgb.R = 0;
+        rgb.G = fX;
+        rgb.B = chroma;
+    }
+    else if (4 <= hueInSixth && hueInSixth < 5) {
+        rgb.R = fX;
+        rgb.G = 0;
+        rgb.B = chroma;
+    }
+    else if (5 <= hueInSixth && hueInSixth < 6) {
+        rgb.R = chroma;
+        rgb.G = 0;
+        rgb.B = fX;
+    }
+    else {
+        rgb.R = 0;
+        rgb.G = 0;
+        rgb.B = 0;
+    }
+
+    rgb.R += baseValue;
+    rgb.G += baseValue;
+    rgb.B += baseValue;
+
+	return rgb;
+}
+
+SColorTemplate<EColorSpace::LinearRGB>::operator SColorTemplate<EColorSpace::HSV>() const
+{
+	return ConvertColor<EColorSpace::LinearRGB, EColorSpace::HSV>(*this);
+}
+
+SColorTemplate<EColorSpace::HSV>::operator SColorTemplate<EColorSpace::LinearRGB>() const
+{
+	return ConvertColor<EColorSpace::HSV, EColorSpace::LinearRGB>(*this);
+}
--- WonderMake/Utilities/Color.h
+++ WonderMake/Utilities/Color.h
@@ -1,29 +1,55 @@
 #pragma once
 
+#include "Rotation.h"
 #include "Imgui/ImguiInclude.h"
 #include "Typedefs.h"
 
-struct SColor
+enum class EColorSpace
+{
+	LinearRGB,
+	HSV
+};
+
+
+template <EColorSpace TColorSpace>
+struct SColorTemplate
+{
+	
+};
+
+template <EColorSpace TFrom, EColorSpace TTo> //requires TFrom != TTo
+SColorTemplate<TTo> ConvertColor(SColorTemplate<TFrom> aFrom) {
+	return SColorTemplate<TTo>();
+};
+
+template <>
+SColorTemplate<EColorSpace::HSV> ConvertColor(SColorTemplate<EColorSpace::LinearRGB> aFrom);
+
+template <>
+SColorTemplate<EColorSpace::LinearRGB> ConvertColor(SColorTemplate<EColorSpace::HSV> aFrom);
+
+template <>
+struct SColorTemplate<EColorSpace::LinearRGB>
 {
 public:
-	constexpr SColor() = default;
+	constexpr SColorTemplate() = default;
 
-	constexpr SColor(const i32 aHexadecimalValue, f32 aAlpha = 1.0f)
-		: SColor(static_cast<u8>((aHexadecimalValue >> 16) & 0xFF), static_cast<u8>((aHexadecimalValue >> 8) & 0xFF), static_cast<u8>((aHexadecimalValue) & 0xFF), (u8)(aAlpha * 255.f))
+	constexpr SColorTemplate(const i32 aHexadecimalValue, f32 aAlpha = 1.0f)
+		: SColorTemplate(static_cast<u8>((aHexadecimalValue >> 16) & 0xFF), static_cast<u8>((aHexadecimalValue >> 8) & 0xFF), static_cast<u8>((aHexadecimalValue) & 0xFF), (u8)(aAlpha * 255.f))
 	{
 
 	}
 
-	constexpr SColor(const u8 aRed, const u8 aGreen, const u8 aBlue, const u8 aAlpha = 255)
-		: SColor(static_cast<f32>(aRed) / 255.f, static_cast<f32>(aGreen) / 255.f, static_cast<f32>(aBlue) / 255.f, static_cast<f32>(aAlpha) / 255.f)
+	constexpr SColorTemplate(const u8 aRed, const u8 aGreen, const u8 aBlue, const u8 aAlpha = 255)
+		: SColorTemplate(static_cast<f32>(aRed) / 255.f, static_cast<f32>(aGreen) / 255.f, static_cast<f32>(aBlue) / 255.f, static_cast<f32>(aAlpha) / 255.f)
 	{
 
 	}
 
-	constexpr SColor(const f32 aRed, const f32 aGreen, const f32 aBlue, const f32 aAlpha = 1.0f) 
+	constexpr SColorTemplate(const f32 aRed, const f32 aGreen, const f32 aBlue, const f32 aAlpha = 1.0f)
 		: R(aRed), G(aGreen), B(aBlue), A(aAlpha)
 	{
-	
+
 	}
 
 	f32 R = 1.0f;
@@ -31,36 +57,66 @@ public:
 	f32 B = 1.0f;
 	f32 A = 1.0f;
 
+	operator SColorTemplate<EColorSpace::HSV>() const;
+
 	operator ImColor() const { return ImColor(R, G, B, A); }
 	operator ImVec4() const { return ImVec4(R, G, B, A); }
 
-	constexpr bool operator==(const SColor& aOther) const { return aOther.R == R && aOther.G == G && aOther.B == B && aOther.A == A; }
-	constexpr bool operator!=(const SColor& aOther) const { return !(aOther == *this); }
+	constexpr bool operator==(const SColorTemplate& aOther) const { return aOther.R == R && aOther.G == G && aOther.B == B && aOther.A == A; }
+	constexpr bool operator!=(const SColorTemplate& aOther) const { return !(aOther == *this); }
 
-	[[nodiscard]] constexpr static SColor CornflowerBlue(const f32 aAlpha = 1.0f)	noexcept { return { 100.f / 255.f, 149.f / 255.f, 237.f / 255.f, aAlpha }; }
-	[[nodiscard]] constexpr static SColor Grey(const f32 aAlpha = 1.0f)				noexcept { return { 0.85f, 0.85f, 0.85f, aAlpha }; }
-	[[nodiscard]] constexpr static SColor White(const f32 aAlpha = 1.0f)			noexcept { return SColor(255_u8, 255_u8, 255_u8, (u8)(aAlpha * 255.f)); }
-	[[nodiscard]] constexpr static SColor Black(const f32 aAlpha = 1.0f)			noexcept { return SColor(0_u8, 0_u8, 0_u8, (u8)(aAlpha * 255.f)); }
-	[[nodiscard]] constexpr static SColor Yellow(const f32 aAlpha = 1.0f)			noexcept { return { 1.0f, 1.0f, 0.0f, aAlpha }; }
-	[[nodiscard]] constexpr static SColor Red(const f32 aAlpha = 1.0f)				noexcept { return { 1.0f, 0.0f, 0.0f, aAlpha }; }
-	[[nodiscard]] constexpr static SColor Green(const f32 aAlpha = 1.0f)			noexcept { return { 0.0f, 1.0f, 0.0f, aAlpha }; }
-	[[nodiscard]] constexpr static SColor Blue(const f32 aAlpha = 1.0f)				noexcept { return { 0.0f, 0.0f, 1.0f, aAlpha }; }
-	[[nodiscard]] constexpr static SColor ImperialRed(const f32 aAlpha = 1.0f)		noexcept { return SColor(0xE54B4B, aAlpha); }
-	[[nodiscard]] constexpr static SColor BlueBell(const f32 aAlpha = 1.0f)			noexcept { return SColor(0x8789C0, aAlpha); }
-	[[nodiscard]] constexpr static SColor KombuGreen(const f32 aAlpha = 1.0f)		noexcept { return SColor(0x334139, aAlpha); }
-	[[nodiscard]] constexpr static SColor LightSalmon(const f32 aAlpha = 1.0f)		noexcept { return SColor(0xFFA987, aAlpha); }
-	[[nodiscard]] constexpr static SColor Seashell(const f32 aAlpha = 1.0f)			noexcept { return SColor(0xF7EBE8, aAlpha); }
-	[[nodiscard]] constexpr static SColor SpanishGray(const f32 aAlpha = 1.0f)		noexcept { return SColor(0x9E9694, aAlpha); }
-	[[nodiscard]] constexpr static SColor Jet(const f32 aAlpha = 1.0f)				noexcept { return SColor(0x333333, aAlpha); }
-	[[nodiscard]] constexpr static SColor RaisinBlack(const f32 aAlpha = 1.0f)		noexcept { return SColor(0x1E1E24, aAlpha); }
-	[[nodiscard]] constexpr static SColor AmaranthPurple(const f32 aAlpha = 1.0f)	noexcept { return SColor(0xA23652, aAlpha); }
-	[[nodiscard]] constexpr static SColor Zomp(const f32 aAlpha = 1.0f)				noexcept { return SColor(0x36A184, aAlpha); }
-	[[nodiscard]] constexpr static SColor MinionYellow(const f32 aAlpha = 1.0f)		noexcept { return SColor(0xF1E254, aAlpha); }
-	[[nodiscard]] constexpr static SColor DimGray(const f32 aAlpha = 1.0f)			noexcept { return SColor(0x6C6C6B, aAlpha); }
-	[[nodiscard]] constexpr static SColor SilverChalice(const f32 aAlpha = 1.0f)	noexcept { return SColor(0xADADAD, aAlpha); }
-	[[nodiscard]] constexpr static SColor Gainsboro(const f32 aAlpha = 1.0f)		noexcept { return SColor(0xE0E0E0, aAlpha); }
-	[[nodiscard]] constexpr static SColor Amaranth(const f32 aAlpha = 1.0f)			noexcept { return SColor(0xE52B50, aAlpha); }
-	[[nodiscard]] constexpr static SColor SeaGreenCrayola(f32 aAlpha = 1.0f)		noexcept { return SColor(0x2AE5BF, aAlpha); }
-	[[nodiscard]] constexpr static SColor MiddleYellow(const f32 aAlpha = 1.0f)		noexcept { return SColor(0xFFE921, aAlpha); }
-	[[nodiscard]] constexpr static SColor EerieBlack(const f32 aAlpha = 1.0f)		noexcept { return SColor(0x151514, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate CornflowerBlue(const f32 aAlpha = 1.0f)		noexcept { return { 100.f / 255.f, 149.f / 255.f, 237.f / 255.f, aAlpha }; }
+	[[nodiscard]] constexpr static SColorTemplate Grey(const f32 aAlpha = 1.0f)					noexcept { return { 0.85f, 0.85f, 0.85f, aAlpha }; }
+	[[nodiscard]] constexpr static SColorTemplate White(const f32 aAlpha = 1.0f)				noexcept { return SColorTemplate(255_u8, 255_u8, 255_u8, (u8)(aAlpha * 255.f)); }
+	[[nodiscard]] constexpr static SColorTemplate Black(const f32 aAlpha = 1.0f)				noexcept { return SColorTemplate(0_u8, 0_u8, 0_u8, (u8)(aAlpha * 255.f)); }
+	[[nodiscard]] constexpr static SColorTemplate Yellow(const f32 aAlpha = 1.0f)				noexcept { return { 1.0f, 1.0f, 0.0f, aAlpha }; }
+	[[nodiscard]] constexpr static SColorTemplate Red(const f32 aAlpha = 1.0f)					noexcept { return { 1.0f, 0.0f, 0.0f, aAlpha }; }
+	[[nodiscard]] constexpr static SColorTemplate Green(const f32 aAlpha = 1.0f)				noexcept { return { 0.0f, 1.0f, 0.0f, aAlpha }; }
+	[[nodiscard]] constexpr static SColorTemplate Blue(const f32 aAlpha = 1.0f)					noexcept { return { 0.0f, 0.0f, 1.0f, aAlpha }; }
+	[[nodiscard]] constexpr static SColorTemplate ImperialRed(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0xE54B4B, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate BlueBell(const f32 aAlpha = 1.0f)				noexcept { return SColorTemplate(0x8789C0, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate KombuGreen(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0x334139, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate LightSalmon(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0xFFA987, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate Seashell(const f32 aAlpha = 1.0f)				noexcept { return SColorTemplate(0xF7EBE8, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate SpanishGray(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0x9E9694, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate Jet(const f32 aAlpha = 1.0f)					noexcept { return SColorTemplate(0x333333, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate RaisinBlack(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0x1E1E24, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate AmaranthPurple(const f32 aAlpha = 1.0f)		noexcept { return SColorTemplate(0xA23652, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate Zomp(const f32 aAlpha = 1.0f)					noexcept { return SColorTemplate(0x36A184, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate MinionYellow(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0xF1E254, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate DimGray(const f32 aAlpha = 1.0f)				noexcept { return SColorTemplate(0x6C6C6B, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate SilverChalice(const f32 aAlpha = 1.0f)		noexcept { return SColorTemplate(0xADADAD, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate Gainsboro(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0xE0E0E0, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate Amaranth(const f32 aAlpha = 1.0f)				noexcept { return SColorTemplate(0xE52B50, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate SeaGreenCrayola(f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0x2AE5BF, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate MiddleYellow(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0xFFE921, aAlpha); }
+	[[nodiscard]] constexpr static SColorTemplate EerieBlack(const f32 aAlpha = 1.0f)			noexcept { return SColorTemplate(0x151514, aAlpha); }
 };
+
+template <>
+struct SColorTemplate<EColorSpace::HSV>
+{
+public:
+	constexpr SColorTemplate() = default;
+
+	constexpr SColorTemplate(const SDegree<f32> aHue, const f32 aSaturation, const f32 aValue, const f32 aAlpha = 1.0f)
+		:Hue(aHue), Saturation(aSaturation), Value(aValue), Alpha(aAlpha)
+	{
+
+	}
+
+	void Rotate(const SDegreeF32 aHueRotation)
+	{
+		Hue += aHueRotation;
+	}
+
+	operator SColorTemplate<EColorSpace::LinearRGB>() const;
+
+	SDegreeF32 Hue = 0.f;
+	f32 Saturation = 0.f;
+	f32 Value = 1.f;
+	f32 Alpha = 1.f;
+};
+
+using SColor = SColorTemplate<EColorSpace::LinearRGB>;
+using SColorHSV = SColorTemplate<EColorSpace::HSV>;

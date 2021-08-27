0e9961e7824650641a172b2eb3dd6a96ace35081
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2021-08-28 00:43:57 +0200

    Added include to WinPlatform. Added undef to GetMessage, max and min.

--- WonderMake/WinPlatform/WinPlatform.h
+++ WonderMake/WinPlatform/WinPlatform.h
@@ -3,4 +3,9 @@
  // TODO: There are some things we should include or define before including Windows.h, Unreal Engine has an example of everything we'll need.
 
 #include <Combaseapi.h>
+#include <shlobj_core.h>
 #include <Windows.h>
+
+#undef GetMessage
+#undef max
+#undef min

809c4d752ef5a1ae72b572bda0917f6c9fca4823
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2021-08-28 18:00:34 +0200

    Added ReadFileJob.

---
+++ WonderMake/FileSystem/ReadFileJob.cpp
@@ -0,0 +1,47 @@
+#include "pch.h"
+#include "ReadFileJob.h"
+
+#include "FileSystem/FileSystem.h"
+
+#include <fstream>
+
+ReadFileJob::ReadFileJob(const FolderLocation aLocation, const std::filesystem::path& aFilePath)
+{
+	const auto dirPath = Get<FileSystem>().GetFolderLocation(aLocation);
+
+	if (!dirPath)
+	{
+		CompleteFailure(ReadFileError::InvalidArguments);
+
+		return;
+	}
+
+	const auto path = *dirPath / aFilePath;
+
+	if (!std::filesystem::exists(path))
+	{
+		CompleteFailure(ReadFileError::FileNotFound);
+
+		return;
+	}
+
+	if (!std::filesystem::is_regular_file(path))
+	{
+		CompleteFailure(ReadFileError::NotAFile);
+
+		return;
+	}
+
+	std::ifstream file(path, std::ios_base::binary);
+
+	if (!file)
+	{
+		CompleteFailure(ReadFileError::FailedToOpen);
+
+		return;
+	}
+
+	Container<u8, Iterable, Indexable, ContiguousElements> buffer(std::istreambuf_iterator<char>(file), {});
+	
+	CompleteSuccess(std::move(buffer));
+}
---
+++ WonderMake/FileSystem/ReadFileJob.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "FileSystem/FileTypes.h"
+#include "Job/Job.h"
+#include "Utilities/Container/Container.h"
+
+class FileSystem;
+
+enum class ReadFileError
+{
+	InvalidArguments,
+	FileNotFound,
+	NotAFile,
+	FailedToOpen
+};
+
+class ReadFileJob
+	: public Job<
+		Policy::Set<
+			PAdd<FileSystem, PWrite>>,
+		JobOutput<Container<u8, Iterable, Indexable, ContiguousElements>>,
+		JobOutputError<ReadFileError>>
+{
+public:
+	ReadFileJob(const FolderLocation aLocation, const std::filesystem::path& aFilePath);
+
+};

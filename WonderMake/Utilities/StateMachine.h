97490cbf75d600a82fdcc97daab389eac7ba7d90
Nicos <nicolas.gustafsson@gmail.com>
2021-10-08 04:37:50 +0200

    Adhd statemachine utility class

---
+++ WonderMake/Utilities/StateMachine.h
@@ -0,0 +1,52 @@
+#pragma once
+
+template<typename TEnumState> requires std::is_enum_v<TEnumState>
+class StateMachine
+{
+public:
+	StateMachine()
+	{
+		SwitchState(TEnumState{});
+	}
+
+	void UpdateStateMachine()
+	{
+		TEnumState newState = EvaluateState();
+
+		if (myCurrentState != newState)
+		{
+			SwitchState(newState);
+		}
+	}
+
+
+	virtual TEnumState EvaluateState() const = 0;
+
+	virtual void OnNewState(const TEnumState aNewState) {}
+
+	f32 GetTimeInState() const
+	{
+		SystemPtr<TimeKeeper> timeKeeper;
+		return timeKeeper->TimeSince(myStateSwitchTime);
+	}
+
+	TEnumState GetCurrentState() const
+	{
+		return myCurrentState;
+	}
+
+private:
+	void SwitchState(const TEnumState aNewState)
+	{
+		SystemPtr<TimeKeeper> timeKeeper;
+
+		myCurrentState = aNewState;
+		myStateSwitchTime = timeKeeper->GetGameTime();
+
+		OnNewState(myCurrentState);
+	}
+	
+	f32 myStateSwitchTime = 0.f;
+	TEnumState myCurrentState;
+};
+

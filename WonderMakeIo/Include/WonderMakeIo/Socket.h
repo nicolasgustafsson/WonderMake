93ed3f835080c3a6ebea3f759a4e0f678ad6ce89
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-01-18 14:13:05 +0100

    Added Socket interface

---
+++ WonderMakeIo/Include/WonderMakeIo/Socket.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include "WonderMakeBase/Result.h"
+#include "WonderMakeBase/Typedefs.h"
+#include "WonderMakeBase/UniqueFunction.h"
+
+#include <vector>
+
+class Socket
+{
+public:
+	enum class EState
+	{
+		Opening,
+		Open,
+		Closing,
+		Closed
+	};
+
+	enum class EAsynchronicity
+	{
+		Asynchronous,
+		Synchronous
+	};
+	enum class EWriteError
+	{
+		InvalidArgs,
+		InvalidState,
+		StateChanged,
+		OutOfMemory,
+		InternalError
+	};
+	enum class EReadError
+	{
+		InvalidState,
+		StateChanged,
+		OutOfMemory,
+		InternalError
+	};
+
+	using OnWriteCallback = UniqueFunction<void(Result<EWriteError>)>;
+	using OnReadCallback = UniqueFunction<void(Result<EReadError, std::vector<u8>>&&)>;
+
+	virtual ~Socket() noexcept = default;
+
+	virtual Result<EWriteError, EAsynchronicity> Write(std::vector<u8> aBuffer, OnWriteCallback&& aOnWrite) = 0;
+	virtual Result<EReadError, EAsynchronicity> Read(OnReadCallback&& aOnRead) = 0;
+	virtual void Close() = 0;
+
+	virtual EState GetState() const noexcept = 0;
+
+protected:
+	Socket() noexcept = default;
+
+};

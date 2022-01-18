3a1a015409441446b868f5f54da3916a900ce9d4
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-01-18 14:14:31 +0100

    Added IpcSystem interface

---
+++ WonderMakeIo/Include/WonderMakeIo/IpcSystem.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "WonderMakeEngine/System.h"
+
+#include <memory>
+
+class IpcAcceptor;
+class IpcConnection;
+
+class IpcSystem
+	: public SystemAbstracted
+{
+public:
+	virtual std::shared_ptr<IpcAcceptor> CreateAcceptor() = 0;
+	virtual std::shared_ptr<IpcConnection> CreateConnection() = 0;
+};

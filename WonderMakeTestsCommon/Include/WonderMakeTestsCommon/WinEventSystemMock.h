aea5e6ca9bfdf0b37ce6b9cc838614561ea7a9cf
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-01-24 14:13:48 +0100

    Added CreateRegisterEvent helper to WinEventSystemMock

--- WonderMakeTestsCommon/Include/WonderMakeTestsCommon/WinEventSystemMock.h
+++ WonderMakeTestsCommon/Include/WonderMakeTestsCommon/WinEventSystemMock.h
@@ -16,4 +16,19 @@ public:
 
 	MOCK_METHOD(void, WaitForEvent, (
 		DWORD aTimeoutMs), (override));
+
+	static auto CreateRegisterEvent()
+	{
+		auto funcPtr = std::make_shared<Closure>([]() {});
+
+		return std::make_pair([funcPtr]()
+			{
+				std::move(*funcPtr)();
+			},
+			[funcPtr](auto, auto&& aCallback)
+			{
+				*funcPtr = std::move(aCallback);
+			});
+	}
+
 };

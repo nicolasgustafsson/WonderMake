334840ac687ddb4e84004fb53b75748e70e9a67e
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-02 15:59:16 +0100

    Added SocketSerializing

---
+++ WonderMakeIo/Include/WonderMakeIo/SocketSerializing.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "WonderMakeIo/Socket.h"
+
+#include "WonderMakeUtility/UniqueFunction.h"
+
+template<typename TSerializable>
+class SocketSerializing
+{
+public:
+	using OnWriteCallback	= UniqueFunction<void(Result<Socket::EWriteError>)>;
+	using OnReadCallback	= UniqueFunction<void(Result<Socket::EReadError, TSerializable>&&)>;
+
+	virtual Result<Socket::EWriteError>	WriteMessage(const TSerializable& aMessage, OnWriteCallback aOnWrite) = 0;
+	virtual Result<Socket::EReadError>	ReadMessage(OnReadCallback aOnRead) = 0;
+
+	virtual void OnClose(Socket::OnCloseCallback aOnClose) = 0;
+	virtual void Close() = 0;
+
+	virtual Socket::EState GetState() const noexcept = 0;
+
+};

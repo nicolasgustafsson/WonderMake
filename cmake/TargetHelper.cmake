93f8333c3e65665c8215c0c7dcddd1597fb8f57f
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-09 11:16:47 +0100

    Targets FOLDER property is now set based on their directory.

---
+++ cmake/TargetHelper.cmake
@@ -0,0 +1,28 @@
+
+function(get_buildsystem_targets ARG_OUT_BUILDSYSTEM_TARGETS ARG_DIRECTORY)
+    set(RESULT_TARGETS)
+
+    get_directory_property(SUBDIRECTORIES DIRECTORY "${ARG_DIRECTORY}" SUBDIRECTORIES)
+
+    foreach(SUBDIR ${SUBDIRECTORIES})
+        get_buildsystem_targets(SUBDIR_TARGETS "${SUBDIR}")
+
+        list(APPEND RESULT_TARGETS ${SUBDIR_TARGETS})
+    endforeach()
+    
+    get_directory_property(BUILDSYSTEM_TARGETS DIRECTORY "${ARG_DIRECTORY}" BUILDSYSTEM_TARGETS)
+
+    foreach(TARGET ${BUILDSYSTEM_TARGETS})
+        list(APPEND RESULT_TARGETS ${TARGET})
+    endforeach()
+
+    set(${ARG_OUT_BUILDSYSTEM_TARGETS} ${RESULT_TARGETS} PARENT_SCOPE)
+endfunction()
+
+function(set_buildsystem_targets_folder ARG_DIRECTORY ARG_FOLDER)
+    get_buildsystem_targets(DIRECTORY_TARGETS ${ARG_DIRECTORY})
+    
+    foreach(DIRECTORY_TARGET IN LISTS DIRECTORY_TARGETS)
+        set_target_properties(${DIRECTORY_TARGET} PROPERTIES FOLDER "${ARG_FOLDER}")
+    endforeach()
+endfunction()

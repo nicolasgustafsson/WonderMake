9723a75cc48aec4ff44d772832e80469babc7d92
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-10 18:34:25 +0100

    Allowed nesting target folders.

--- cmake/TargetHelper.cmake
+++ cmake/TargetHelper.cmake
@@ -23,6 +23,12 @@ function(set_buildsystem_targets_folder ARG_DIRECTORY ARG_FOLDER)
     get_buildsystem_targets(DIRECTORY_TARGETS ${ARG_DIRECTORY})
     
     foreach(DIRECTORY_TARGET IN LISTS DIRECTORY_TARGETS)
-        set_target_properties(${DIRECTORY_TARGET} PROPERTIES FOLDER "${ARG_FOLDER}")
+        get_target_property(TARGET_FOLDER ${DIRECTORY_TARGET} FOLDER)
+
+        if("${TARGET_FOLDER}" STREQUAL "TARGET_FOLDER-NOTFOUND")
+            set(TARGET_FOLDER "")
+        endif()
+
+        set_target_properties(${DIRECTORY_TARGET} PROPERTIES FOLDER "${ARG_FOLDER}/${TARGET_FOLDER}")
     endforeach()
 endfunction()

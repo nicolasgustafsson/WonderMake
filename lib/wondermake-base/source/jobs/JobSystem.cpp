b1072852d55755c2750b4a4382c8d981a8f13a4e
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-17 11:16:05 +0100

    Added refactored JobSystem.

---
+++ lib/wondermake-base/source/jobs/JobSystem.cpp
@@ -0,0 +1,12 @@
+#include "wondermake-base/jobs/JobSystem.h"
+
+namespace jobs_refactor // Temporary until refactor is done.
+{
+
+JobSystem::JobSystem(JobRegistry aJobRegistry, SystemContainer& aSystemContainer, AnyExecutor aExecutor) noexcept
+	: myJobRegistry(std::move(aJobRegistry))
+	, mySystemContainer(aSystemContainer)
+	, myExecutor(std::move(aExecutor))
+{}
+
+}

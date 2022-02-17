9df269823efaa781844ebe58d4213d86108b5186
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-17 15:49:20 +0100

    Added AnyExecutor.

---
+++ lib/wondermake-utility/include/wondermake-utility/AnyExecutor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "wondermake-utility/Executor.h"
+
+#include <functional>
+#include <type_traits>
+
+class AnyExecutor
+{
+public:
+	AnyExecutor() noexcept = default;
+	template<CExecutor TExecutor>
+	inline AnyExecutor(TExecutor&& aExecutor) noexcept requires(!std::is_same_v<AnyExecutor, std::decay_t<TExecutor>>)
+	{
+		myExecute = [executor = std::forward<TExecutor>(aExecutor)](Closure&& aClosure)
+		{
+			executor.Execute(std::move(aClosure));
+		};
+	}
+	AnyExecutor(const AnyExecutor&) noexcept = default;
+	AnyExecutor(AnyExecutor&&) noexcept = default;
+
+	void Execute(Closure&& aClosure) const;
+
+private:
+	std::function<void(Closure&&)> myExecute;
+
+};

0968cd6b2d409a2315487e45ef5f6ae312b17d85
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-18 14:47:49 +0100

    CreateObjectJob now uses the refactored job.

--- lib/wondermake/Object/CreateObjectJob.h
+++ lib/wondermake/Object/CreateObjectJob.h
@@ -1,21 +1,22 @@
 #pragma once
 
 #include "wondermake-base/Cargo.h"
-#include "wondermake-base/Job.h"
+#include "wondermake-base/jobs/JobBase.h"
 
 #include "Object/Object.h"
 
 #include "Functionalities/FunctionalitySystem.h"
 
-#include <tuple>
+#include <pair>
 
 namespace _Impl
 {
 	template<typename... TFunctionalities>
-	using _CreateObjectJobType = Job<
+	using _CreateObjectJobType = jobs_refactor::Job<
+		jobs_refactor::NoJobInput,
+		std::pair<Object, Cargo<TFunctionalities...>>
 		Policy::Set<
-			PAdd<FunctionalitySystemDelegate<std::decay_t<TFunctionalities>>, PWrite>...>,
-		JobOutput<Object&&, Cargo<TFunctionalities...>&&>>;
+			PAdd<FunctionalitySystemDelegate<std::decay_t<TFunctionalities>>, PWrite>...>>;
 }
 
 // TODO(Kevin): Make require write access for inherited policies. Do this by spawning smaller jobs.
@@ -24,9 +25,9 @@ class CreateObjectJob final
 	: public _Impl::_CreateObjectJobType<TFunctionalities...>
 {
 public:
-	inline CreateObjectJob() noexcept
+	inline void Run(Promise<std::pair<Object, Cargo<TFunctionalities...>>> aPromise) noexcept
 	{
-		_Impl::_CreateObjectJobType<TFunctionalities...>::CompleteSuccess(std::move(myObject), WrapCargo(AddFunctionalityHelper<TFunctionalities>()...));
+		aPromise.Complete(std::make_pair(std::move(myObject), WrapCargo(AddFunctionalityHelper<TFunctionalities>()...)));
 	}
 
 private:

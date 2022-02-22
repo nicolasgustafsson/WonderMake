29f895b720d749c10f3fc7fe4c74ca7b0b76b7e3
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-22 15:39:11 +0100

    Added WriteFileJob.

---
+++ lib/wondermake-io/include/wondermake-io/WriteFileJob.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "wondermake-io/FileTypes.h"
+
+#include "wondermake-base/JobBase.h"
+
+#include "wondermake-utility/Result.h"
+
+#include <filesystem>
+#include <vector>
+
+class FileSystem;
+
+enum class EWriteFileError
+{
+	InvalidArguments,
+	PathIsAlreadyADirectory,
+	FailedToCreateDirectory,
+	FailedToOpen
+};
+
+class WriteFileJob
+	: public JobAbstracted<
+		JobInputSet<
+			JobInput<FolderLocation, std::filesystem::path, std::vector<u8>>,
+			JobInput<FolderLocation, std::filesystem::path, std::string>>,
+		Result<EWriteFileError>>
+{};

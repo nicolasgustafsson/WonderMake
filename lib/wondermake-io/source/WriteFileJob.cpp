cba64649ba91b3e9f480e3c1b8eefd0f04f06171
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-02-22 15:39:11 +0100

    Added WriteFileJob.

---
+++ lib/wondermake-io/source/WriteFileJob.cpp
@@ -0,0 +1,77 @@
+#include "wondermake-io/WriteFileJob.h"
+
+#include "wondermake-io/FileSystem.h"
+
+#include "wondermake-base/JobGlobal.h"
+
+#include <fstream>
+
+class WriteFileJobImpl
+	: public JobSub<
+		Policy::Set<
+			PAdd<FileSystem, PWrite>>>
+	, public WriteFileJob
+{
+public:
+	void Run(Promise<Output> aPromise, FolderLocation aLocation, std::filesystem::path aFilePath, std::vector<u8> aBuffer) override
+	{
+		std::filesystem::path path;
+
+		if (aFilePath.is_relative())
+		{
+			const auto dirPath = Get<FileSystem>().GetFolderLocation(aLocation);
+
+			if (!dirPath)
+			{
+				aPromise.Complete(EWriteFileError::InvalidArguments);
+
+				return;
+			}
+
+			path = *dirPath / aFilePath;
+		}
+		else
+		{
+			path = std::move(aFilePath);
+		}
+
+		if (std::filesystem::is_directory(path))
+		{
+			aPromise.Complete(EWriteFileError::PathIsAlreadyADirectory);
+
+			return;
+		}
+
+		if (!std::filesystem::is_directory(path.parent_path())
+			&& !std::filesystem::create_directories(path.parent_path()))
+		{
+			aPromise.Complete(EWriteFileError::FailedToCreateDirectory);
+
+			return;
+		}
+
+		std::ofstream file(path, std::ios_base::binary);
+
+		if (!file)
+		{
+			aPromise.Complete(EWriteFileError::FailedToOpen);
+
+			return;
+		}
+
+		file.write(reinterpret_cast<const char*>(aBuffer.data()), aBuffer.size());;
+	
+		file.flush();
+
+		file.close();
+
+		aPromise.Complete(Success);
+	}
+	void Run(Promise<Output> aPromise, FolderLocation aLocation, std::filesystem::path aFilePath, std::string aBuffer) override
+	{
+		Run(std::move(aPromise), aLocation, std::move(aFilePath), std::vector<u8>(aBuffer.begin(), aBuffer.end()));
+	}
+
+};
+
+WM_REGISTER_JOB_MASKED(WriteFileJobImpl, WriteFileJob);

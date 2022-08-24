d336e787513aa9a39b45f9d44cd2d69860a9fabe
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-08-24 12:44:01 +0200

    Refactored Result.

--- lib/wondermake-utility/include/wondermake-utility/Result.h
+++ lib/wondermake-utility/include/wondermake-utility/Result.h
@@ -2,87 +2,342 @@
 
 #include "Typedefs.h"
 
-#include <optional>
-#include <type_traits>
+#include <any>
 #include <variant>
 
-struct _SuccessType
+template<typename TType>
+struct ResultSuccess final
 {
-	constexpr [[nodiscard]] bool operator==(const _SuccessType&) const noexcept
-	{
-		return true;
-	}
+	TType Value;
 };
-struct _FailureType
+template<>
+struct ResultSuccess<void> final {};
+
+template<typename TType>
+struct ResultFailure final
 {
-	constexpr [[nodiscard]] bool operator==(const _SuccessType&) const noexcept
+	TType Value;
+};
+template<>
+struct ResultFailure<void> final {};
+
+template<typename TArg>
+constexpr ResultSuccess<std::decay_t<TArg>> Ok(TArg&& aValue)
+{
+	return ResultSuccess<std::decay_t<TArg>>{ std::forward<TArg>(aValue) };
+}
+template<typename TArg>
+constexpr ResultFailure<std::decay_t<TArg>> Err(TArg&& aValue)
+{
+	return ResultFailure<std::decay_t<TArg>>{ std::forward<TArg>(aValue) };
+}
+
+constexpr [[nodiscard]] ResultSuccess<void> Ok()
+{
+	return ResultSuccess<void>{};
+}
+constexpr [[nodiscard]] ResultFailure<void> Err()
+{
+	return ResultFailure<void>{};
+}
+
+namespace ResultConversions
+{
+	template<typename TReturnType, typename... TArgs>
+	constexpr bool Has(const std::variant<TArgs...>& aValue)
 	{
-		return true;
+		static_assert((std::is_same_v<TReturnType, TArgs> || ...), "Type is not part of variant.");
+
+		return std::holds_alternative<TReturnType>(aValue);
 	}
+	template<typename TReturnType, typename... TArgs>
+	constexpr TReturnType& Get(std::variant<TArgs...>& aValue)
+	{
+		static_assert((std::is_same_v<TReturnType, TArgs> || ...), "Type is not part of variant.");
+
+		return std::get<TReturnType>(aValue);
+	}
+	
+	template<typename TReturnType>
+	bool Has(const std::any& aValue)
+	{
+		return aValue.has_value() && aValue.type() == typeid(TReturnType);
+	}
+	template<typename TReturnType>
+	TReturnType& Get(std::any& aValue)
+	{
+		return std::any_cast<TReturnType&>(aValue);
+	}
+}
+
+template<typename TType, typename TReturnType>
+concept ResultGettableResult = requires(TType& aValue, const TType& aConstValue)
+{
+	{ ResultConversions::Has<TReturnType>(aConstValue) } -> std::convertible_to<bool>;
+	{ ResultConversions::Get<TReturnType>(aValue) } -> std::convertible_to<TReturnType&>;
 };
 
-constexpr _SuccessType Success;
-constexpr _FailureType Failure;
+template<typename TSuccessType, typename TErrorType, typename TRhsSuccessType, typename TRhsErrorType>
+concept ConvertableResultNoThrow = 
+	((std::is_void_v<TSuccessType> && std::is_void_v<TRhsSuccessType>) || std::is_nothrow_constructible_v<TSuccessType, const TRhsSuccessType&>) &&
+	((std::is_void_v<TErrorType> && std::is_void_v<TRhsErrorType>) || std::is_nothrow_constructible_v<TErrorType, const TRhsErrorType&>);
+template<typename TSuccessType, typename TErrorType, typename TRhsSuccessType, typename TRhsErrorType>
+concept ConvertableResultRValueNoThrow =
+	((std::is_void_v<TSuccessType> && std::is_void_v<TRhsSuccessType>) || std::is_nothrow_constructible_v<TSuccessType, TRhsSuccessType&&>) &&
+	((std::is_void_v<TErrorType> && std::is_void_v<TRhsErrorType>) || std::is_nothrow_constructible_v<TErrorType, TRhsErrorType&&>);
 
-template<typename TErrorType, typename TSuccessType = _SuccessType, typename TMeta = u32>
-class Result
+template<typename TSuccessType, typename TErrorType, typename TRhsSuccessType, typename TRhsErrorType>
+concept ConvertableResult = 
+	((std::is_void_v<TSuccessType> && std::is_void_v<TRhsSuccessType>) || std::is_constructible_v<TSuccessType, const TRhsSuccessType&>) &&
+	((std::is_void_v<TErrorType> && std::is_void_v<TRhsErrorType>) || std::is_constructible_v<TErrorType, const TRhsErrorType&>);
+template<typename TSuccessType, typename TErrorType, typename TRhsSuccessType, typename TRhsErrorType>
+concept ConvertableResultRValue = 
+	((std::is_void_v<TSuccessType> && std::is_void_v<TRhsSuccessType>) || std::is_constructible_v<TSuccessType, TRhsSuccessType&&>) &&
+	((std::is_void_v<TErrorType> && std::is_void_v<TRhsErrorType>) || std::is_constructible_v<TErrorType, TRhsErrorType&&>);
+
+template<typename TSuccessType, typename TErrorType = void>
+class Result final
 {
+private:
+	inline static constexpr size_t IndexSuccess	= 0;
+	inline static constexpr size_t IndexError	= 1;
+
 public:
-	constexpr Result(TSuccessType aSuccess, TMeta aMetaValue = TMeta()) noexcept(std::is_nothrow_move_constructible_v<TSuccessType>)
-		: myResult(std::move(aSuccess))
-		, myMetaValue(aMetaValue)
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	constexpr Result(const Result<TRhsSuccessType, TRhsErrorType>& aResult)
+		noexcept(ConvertableResultNoThrow<TSuccessType, TErrorType, TRhsSuccessType, TRhsErrorType>)
+		requires(ConvertableResult<TSuccessType, TErrorType, TRhsSuccessType, TRhsErrorType>)
+		: myResult(MakeResult(aResult))
 	{}
-	constexpr Result(TErrorType aError, TMeta aMetaValue = TMeta()) noexcept(std::is_nothrow_move_constructible_v<TErrorType>)
-		: myResult(std::move(aError))
-		, myMetaValue(aMetaValue)
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	constexpr Result(Result<TRhsSuccessType, TRhsErrorType>&& aResult)
+		noexcept(ConvertableResultRValueNoThrow<TSuccessType, TErrorType, TRhsSuccessType&&, TRhsErrorType&&>)
+		requires(ConvertableResultRValue<TSuccessType, TErrorType, TRhsSuccessType&&, TRhsErrorType&&>)
+		: myResult(MakeResult(std::move(aResult)))
+	{}
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	constexpr Result(const Result<TRhsSuccessType, TRhsErrorType>& aResult)
+		requires(!std::is_constructible_v<TSuccessType, const TRhsSuccessType&> && std::is_constructible_v<TErrorType, const TRhsErrorType&>)
+		: myResult(std::in_place_index<IndexError>, aResult.Err())
+	{}
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	constexpr Result(Result<TRhsSuccessType, TRhsErrorType>&& aResult)
+		requires(!std::is_constructible_v<TSuccessType, TRhsSuccessType&&> && std::is_constructible_v<TErrorType, TRhsErrorType&&>)
+		: myResult(std::in_place_index<IndexError>, std::move(aResult).Err())
+	{}
+	
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	constexpr Result(const Result<TRhsSuccessType, TRhsErrorType>& aResult)
+		requires(!std::is_constructible_v<TErrorType, const TRhsErrorType&> && std::is_constructible_v<TSuccessType, const TRhsSuccessType&>)
+		: myResult(std::in_place_index<IndexSuccess>, aResult.Unwrap())
+	{}
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	constexpr Result(Result<TRhsSuccessType, TRhsErrorType>&& aResult)
+		requires(!std::is_constructible_v<TErrorType, TRhsErrorType&&> && std::is_constructible_v<TSuccessType, TRhsSuccessType&&>)
+		: myResult(std::in_place_index<IndexSuccess>, std::move(aResult).Unwrap())
+	{}
+	
+	template<typename TArg>
+	constexpr Result(ResultSuccess<TArg> aValue) noexcept(std::is_nothrow_constructible_v<TSuccessType, TArg&&>)
+		requires(!std::is_void_v<TSuccessType> && std::is_constructible_v<TSuccessType, TArg&&>)
+		: myResult(std::in_place_index<IndexSuccess>, std::move(aValue.Value))
+	{}
+	template<typename TArg>
+	constexpr Result(ResultFailure<TArg> aValue) noexcept(std::is_nothrow_constructible_v<TErrorType, TArg&&>)
+		requires(!std::is_void_v<TErrorType> && std::is_constructible_v<TErrorType, TArg&&>)
+		: myResult(std::in_place_index<IndexError>, std::move(aValue.Value))
+	{}
+	
+	constexpr Result(ResultSuccess<void> aValue) noexcept
+		requires(std::is_void_v<TSuccessType>)
+		: myResult(std::in_place_index<IndexSuccess>, aValue)
+	{}
+	constexpr Result(ResultFailure<void> aValue) noexcept
+		requires(std::is_void_v<TErrorType>)
+		: myResult(std::in_place_index<IndexError>, aValue)
 	{}
 
-	constexpr [[nodiscard]] bool operator==(const Result& aRhs) const noexcept
+	constexpr [[nodiscard]] bool operator==(const Result&) const noexcept = default;
+	constexpr [[nodiscard]] bool operator!=(const Result&) const noexcept = default;
+
+	constexpr [[nodiscard]] operator bool() const noexcept
 	{
-		return (std::holds_alternative<TErrorType>(myResult) && std::holds_alternative<TErrorType>(aRhs.myResult) && std::get<TErrorType>(myResult) == std::get<TErrorType>(aRhs.myResult))
-			|| (std::holds_alternative<TSuccessType>(myResult) && std::holds_alternative<TSuccessType>(aRhs.myResult) && std::get<TSuccessType>(myResult) == std::get<TSuccessType>(aRhs.myResult));
-	}
-	constexpr [[nodiscard]] bool operator==(const TSuccessType& aRhs) const noexcept
-	{
-		return std::holds_alternative<TSuccessType>(myResult)
-			&& std::get<TSuccessType>(myResult) == aRhs;
-	}
-	constexpr [[nodiscard]] bool operator==(const TErrorType& aRhs) const noexcept
-	{
-		return std::holds_alternative<TErrorType>(myResult)
-			&& std::get<TErrorType>(myResult) == aRhs;
+		return IsOk();
 	}
 
-	constexpr [[nodiscard]] operator bool() const
+	constexpr [[nodiscard]] bool IsOk() const noexcept
 	{
-		return std::holds_alternative<TSuccessType>(myResult);
+		return myResult.index() == IndexSuccess;
 	}
+	constexpr [[nodiscard]] bool IsErr() const noexcept
+	{
+		return myResult.index() == IndexError;
+	}
+	
+	constexpr [[nodiscard]] const auto& Unwrap() const& noexcept
+		requires(!std::is_void_v<TSuccessType>)
+	{
+		return std::get<IndexSuccess>(myResult).Value;
+	}
+	constexpr [[nodiscard]] auto& Unwrap() & noexcept
+		requires(!std::is_void_v<TSuccessType>)
+	{
+		return std::get<IndexSuccess>(myResult).Value;
+	}
+	constexpr [[nodiscard]] auto Unwrap() &&
+		requires(!std::is_void_v<TSuccessType>)
+	{
+		return std::move(std::get<IndexSuccess>(myResult).Value);
+	}
+	
+	template<typename TArg>
+	constexpr [[nodiscard]] TSuccessType UnwrapOr(TArg&& aArg) const noexcept(std::is_nothrow_constructible_v<TSuccessType, decltype(std::forward<TArg>(aArg))> && std::is_nothrow_copy_constructible_v<TSuccessType>)
+		requires(!std::is_void_v<TSuccessType> && std::is_constructible_v<TSuccessType, decltype(std::forward<TArg>(aArg))> && std::is_copy_constructible_v<TSuccessType>)
+	{
+		if (myResult.index() != IndexSuccess)
+			return std::forward<TArg>(aArg);
 
-	constexpr [[nodiscard]] operator TErrorType() const requires(std::is_copy_constructible_v<TErrorType>&& std::is_copy_assignable_v<TErrorType>)
-	{
-		return std::get<TErrorType>(myResult);
+		return std::get<IndexSuccess>(myResult).Value;
 	}
-	constexpr [[nodiscard]] operator TErrorType()&&
+	template<typename TArg>
+	constexpr [[nodiscard]] TSuccessType UnwrapOr(TArg&& aArg) && noexcept(std::is_nothrow_constructible_v<TSuccessType, decltype(std::forward<TArg>(aArg))>)
+		requires(!std::is_void_v<TSuccessType> && std::is_constructible_v<TSuccessType, decltype(std::forward<TArg>(aArg))>)
 	{
-		return std::move(std::get<TErrorType>(myResult));
-	}
+		if (myResult.index() != IndexSuccess)
+			return std::forward<TArg>(aArg);
 
-	constexpr [[nodiscard]] operator TSuccessType() const requires(std::is_copy_constructible_v<TSuccessType> && std::is_copy_assignable_v<TSuccessType>)
-	{
-		return std::get<TSuccessType>(myResult);
+		return std::move(std::get<IndexSuccess>(myResult).Value);
 	}
-	constexpr [[nodiscard]] operator TSuccessType() &&
+	
+	constexpr [[nodiscard]] const auto& Err() const&
+		requires(!std::is_void_v<TErrorType>)
 	{
-		return std::move(std::get<TSuccessType>(myResult));
+		return std::get<IndexError>(myResult).Value;
 	}
+	constexpr [[nodiscard]] auto& Err() &
+		requires(!std::is_void_v<TErrorType>)
+	{
+		return std::get<IndexError>(myResult).Value;
+	}
+	constexpr [[nodiscard]] auto Err() &&
+		requires(!std::is_void_v<TErrorType>)
+	{
+		return std::move(std::get<IndexError>(myResult).Value);
+	}
+	
+	class ResultWrapper
+	{
+	public:
+		constexpr ResultWrapper(std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>& aResult)
+			: myResult(aResult)
+		{}
 
-	constexpr [[nodiscard]] TMeta Meta() const
+		template<typename TType = TSuccessType, typename TCallable>
+		constexpr ResultWrapper AndThen(TCallable&& aCallable)
+		{
+			if (myResult.index() != IndexSuccess)
+				return myResult;
+
+			static_assert(std::is_void_v<TType> || std::is_same_v<TType, TSuccessType> || ResultGettableResult<TSuccessType, TType>);
+
+			if constexpr (std::is_void_v<TType>)
+				(void)std::forward<TCallable>(aCallable)();
+			else if constexpr (std::is_same_v<TType, TSuccessType>)
+				(void)std::forward<TCallable>(aCallable)(Unwrap());
+			else if constexpr (ResultGettableResult<TSuccessType, TType>)
+			{
+				if (ResultConversions::Has<TType>(Unwrap()))
+					(void)std::forward<TCallable>(aCallable)(ResultConversions::Get<TType>(Unwrap()));
+			}
+
+			return myResult;
+		}
+		template<typename TType = TErrorType, typename TCallable>
+		constexpr ResultWrapper OrElse(TCallable&& aCallable)
+		{
+			if (myResult.index() != IndexError)
+				return myResult;
+
+			static_assert(std::is_void_v<TType> || std::is_same_v<TType, TErrorType> || ResultGettableResult<TErrorType, TType>);
+
+			if constexpr (std::is_void_v<TType>)
+				(void)std::forward<TCallable>(aCallable)();
+			else if constexpr (std::is_same_v<TType, TErrorType>)
+				(void)std::forward<TCallable>(aCallable)(Err());
+			else if constexpr (ResultGettableResult<TErrorType, TType>)
+			{
+				if (ResultConversions::Has<TType>(Err()))
+					(void)std::forward<TCallable>(aCallable)(ResultConversions::Get<TType>(Err()));
+			}
+
+			return myResult;
+		}
+
+	private:
+		constexpr auto& Unwrap()
+			requires(!std::is_void_v<TSuccessType>)
+		{
+			return std::get<IndexSuccess>(myResult).Value;
+		};
+		constexpr auto& Err()
+			requires(!std::is_void_v<TErrorType>)
+		{
+			return std::get<IndexError>(myResult).Value;
+		};
+
+		std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>& myResult;
+
+	};
+
+	template<typename TType = TSuccessType, typename TCallable>
+	constexpr ResultWrapper AndThen(TCallable&& aCallable)
 	{
-		return myMetaValue;
+		return ResultWrapper(myResult).AndThen<TType>(std::forward<TCallable>(aCallable));
+	}
+	template<typename TType = TErrorType, typename TCallable>
+	constexpr ResultWrapper OrElse(TCallable&& aCallable)
+	{
+		return ResultWrapper(myResult).OrElse<TType>(std::forward<TCallable>(aCallable));
 	}
 
 private:
-	std::variant<TErrorType, TSuccessType> myResult;
-	TMeta myMetaValue;
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	inline static std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>> MakeResult(const Result<TRhsSuccessType, TRhsErrorType>& aResult)
+	{
+		if (aResult)
+		{
+			if constexpr (std::is_void_v<TSuccessType>)
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexSuccess>);
+			else
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexSuccess>, aResult.Unwrap());
+		}
+		else
+		{
+			if constexpr (std::is_void_v<TErrorType>)
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexError>);
+			else
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexError>, aResult.Err());
+		}
+	}
+	template<typename TRhsSuccessType, typename TRhsErrorType>
+	inline static std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>> MakeResult(Result<TRhsSuccessType, TRhsErrorType>&& aResult)
+	{
+		if (aResult)
+		{
+			if constexpr (std::is_void_v<TSuccessType>)
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexSuccess>);
+			else
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexSuccess>, std::move(aResult).Unwrap());
+		}
+		else
+		{
+			if constexpr (std::is_void_v<TErrorType>)
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexError>);
+			else
+				return std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>>(std::in_place_index<IndexError>, std::move(aResult).Err());
+		}
+	}
+
+	std::variant<ResultSuccess<TSuccessType>, ResultFailure<TErrorType>> myResult;
 
 };

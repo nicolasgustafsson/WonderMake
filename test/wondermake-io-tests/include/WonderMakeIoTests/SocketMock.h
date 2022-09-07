f0e98772acf928f0bd6cd61816140ea1f02e53a9
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-09-07 12:43:20 +0200

    Updated SocketMock to use Futures.

--- test/wondermake-io-tests/include/WonderMakeIoTests/SocketMock.h
+++ test/wondermake-io-tests/include/WonderMakeIoTests/SocketMock.h
@@ -8,50 +8,26 @@ class SocketMock
 	: public Socket
 {
 public:
-	using ResultTypeWrite	= Result<EAsynchronicity, SWriteError>;
-	using ResultTypeRead	= Result<EAsynchronicity, SReadError>;
-
-	MOCK_METHOD(ResultTypeWrite, Write, (std::vector<u8> aBuffer, OnWriteCallback aOnWrite), (override));
-	MOCK_METHOD(ResultTypeRead, Read, (OnReadCallback aOnRead), (override));
-	MOCK_METHOD(void, OnClose, (OnCloseCallback aOnClose), (override));
-	MOCK_METHOD(void, Close, (), (override));
+	MOCK_METHOD(FutureTypeWrite, Write, (std::vector<u8> aBuffer), (override));
+	MOCK_METHOD(FutureTypeRead, Read, (), (override));
+	MOCK_METHOD(FutureTypeClose, OnClose, (), (override));
+	MOCK_METHOD(FutureTypeClose, Close, (), (override));
 
 	MOCK_METHOD(EState, GetState, (), (const, noexcept, override));
 
 	void DelegateToFake()
 	{
 		ON_CALL(*this, Write)
-			.WillByDefault(Return(Err(SWriteError{ EWriteError::InternalError })));
+			.WillByDefault(Return(MakeCompletedFuture<ResultTypeWrite>(Err(SWriteError{ EWriteError::InternalError }))));
 		ON_CALL(*this, Read)
-			.WillByDefault(Return(Err(SReadError{ EReadError::InternalError })));
+			.WillByDefault(Return(MakeCompletedFuture<ResultTypeRead>(Err(SReadError{ EReadError::InternalError }))));
+		ON_CALL(*this, OnClose)
+			.WillByDefault(Return(MakeCompletedFuture<ResultTypeClose>(Err(SCloseError{ ECloseError::InternalError }))));
+		ON_CALL(*this, Close)
+			.WillByDefault(Return(MakeCompletedFuture<ResultTypeClose>(Err(SCloseError{ ECloseError::InternalError }))));
 
 		ON_CALL(*this, GetState)
 			.WillByDefault(Return(EState::Closed));
-	}
-};
-
-class SocketCallbackMock
-{
-public:
-	using ArgTypeOnWrite = Result<void, Socket::SWriteError>;
-	using ArgTypeOnRead = Result<std::vector<u8>, Socket::SReadError>&&;
-	using ArgTypeOnClose = Result<Socket::SCloseLocation, Socket::SCloseError>;
-
-	MOCK_METHOD(void, OnWrite, (ArgTypeOnWrite));
-	MOCK_METHOD(void, OnRead, (ArgTypeOnRead));
-	MOCK_METHOD(void, OnClose, (ArgTypeOnClose));
-
-	auto CreateOnWrite()
-	{
-		return [this](auto&& aResult) { OnWrite(std::move(aResult)); };
-	}
-	auto CreateOnRead()
-	{
-		return [this](auto&& aResult) { OnRead(std::move(aResult)); };
-	}
-	auto CreateOnClose()
-	{
-		return [this](auto&& aResult) { OnClose(std::move(aResult)); };
 	}
 };
 

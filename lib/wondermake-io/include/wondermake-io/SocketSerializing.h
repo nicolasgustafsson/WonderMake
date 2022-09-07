444c9f822405d565ef812a32154ce9cfd96dae6c
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-09-07 15:56:23 +0200

    Updated SocketSerializing to use Futures.

--- lib/wondermake-io/include/wondermake-io/SocketSerializing.h
+++ lib/wondermake-io/include/wondermake-io/SocketSerializing.h
@@ -2,22 +2,27 @@
 
 #include "wondermake-io/Socket.h"
 
-#include "wondermake-utility/UniqueFunction.h"
-
 template<typename TSerializable>
 class SocketSerializing
 {
 public:
+	using ResultTypeWrite	= Result<void, Socket::SWriteError>;
+	using ResultTypeRead	= Result<TSerializable, Socket::SReadError>;
+	using ResultTypeClose	= Result<Socket::SCloseLocation, Socket::SCloseError>;
+
+	using FutureTypeWrite	= Future<ResultTypeWrite>;
+	using FutureTypeRead	= Future<ResultTypeRead>;
+	using FutureTypeClose	= Future<ResultTypeClose>;
+
 	using OnWriteCallback	= UniqueFunction<void(Result<void, Socket::SWriteError>)>;
 	using OnReadCallback	= UniqueFunction<void(Result<TSerializable, Socket::SReadError>&&)>;
 
 	virtual ~SocketSerializing() noexcept = default;
 
-	virtual Result<void, Socket::SWriteError>	WriteMessage(const TSerializable& aMessage, OnWriteCallback aOnWrite) = 0;
-	virtual Result<void, Socket::SReadError>	ReadMessage(OnReadCallback aOnRead) = 0;
-
-	virtual void OnClose(Socket::OnCloseCallback aOnClose) = 0;
-	virtual void Close() = 0;
+	virtual FutureTypeWrite	WriteMessage(const TSerializable& aMessage) = 0;
+	virtual FutureTypeRead	ReadMessage() = 0;
+	virtual FutureTypeClose	OnClose() = 0;
+	virtual FutureTypeClose	Close() = 0;
 
 	virtual Socket::EState GetState() const noexcept = 0;
 

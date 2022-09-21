c77fcf25ce44bf9a25fdc1682190b5fa252ff569
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-09-21 19:23:43 +0200

    Added TagWmConfigurationRemote.

--- lib/wondermake-base/include/wondermake-base/WmLogTags.h
+++ lib/wondermake-base/include/wondermake-base/WmLogTags.h
@@ -2,15 +2,16 @@
 
 #include "wondermake-base/LogTag.h"
 
-inline constexpr auto TagWonderMake			= MakeLogTag("WonderMake");
+inline constexpr auto TagWonderMake				= MakeLogTag("WonderMake");
 
-inline constexpr auto TagWmConfiguration	= MakeLogTag("WmConfiguration");
-inline constexpr auto TagWmJobSystem		= MakeLogTag("WmJobSystem");
-inline constexpr auto TagWmLoggerRemote		= MakeLogTag("WmLoggerRemote");
+inline constexpr auto TagWmConfiguration		= MakeLogTag("WmConfiguration");
+inline constexpr auto TagWmJobSystem			= MakeLogTag("WmJobSystem");
+inline constexpr auto TagWmLoggerRemote			= MakeLogTag("WmLoggerRemote");
+inline constexpr auto TagWmConfigurationRemote	= MakeLogTag("WmConfigurationRemote");
 
-inline constexpr auto TagWmCollision		= MakeLogTag("WmCollision");
-inline constexpr auto TagWmOpenGL			= MakeLogTag("WmOpenGL");
-inline constexpr auto TagWmRendering		= MakeLogTag("WmRendering");
-inline constexpr auto TagWmNodeGraph		= MakeLogTag("WmNodeGraph");
-inline constexpr auto TagWmScheduler		= MakeLogTag("WmScheduler");
-inline constexpr auto TagWmResources		= MakeLogTag("WmResources");
+inline constexpr auto TagWmCollision			= MakeLogTag("WmCollision");
+inline constexpr auto TagWmOpenGL				= MakeLogTag("WmOpenGL");
+inline constexpr auto TagWmRendering			= MakeLogTag("WmRendering");
+inline constexpr auto TagWmNodeGraph			= MakeLogTag("WmNodeGraph");
+inline constexpr auto TagWmScheduler			= MakeLogTag("WmScheduler");
+inline constexpr auto TagWmResources			= MakeLogTag("WmResources");

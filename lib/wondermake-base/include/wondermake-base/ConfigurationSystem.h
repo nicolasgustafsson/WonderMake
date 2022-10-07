d8eac1f8a8d821f290297cc9356b476c9a19fc39
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-10-07 17:39:56 +0200

    Added override change subscription to ConfigurationSystem.

--- lib/wondermake-base/include/wondermake-base/ConfigurationSystem.h
+++ lib/wondermake-base/include/wondermake-base/ConfigurationSystem.h
@@ -4,6 +4,10 @@
 #include "wondermake-base/ProcessId.h"
 #include "wondermake-base/System.h"
 
+#include "wondermake-utility/BufferExecutor.h"
+#include "wondermake-utility/CancelableList.h"
+#include "wondermake-utility/EventSubscriber.h"
+
 #include <unordered_map>
 
 class ConfigurationSystem
@@ -11,10 +15,13 @@ class ConfigurationSystem
 		Policy::Set<>,
 		SystemTraits::Set<
 			STFoundational,
-			STThreadsafe,
 			STSingleton>>
 {
 public:
+	inline ConfigurationSystem() noexcept
+		: myEventList(GetExecutor())
+	{}
+
 	template<CConfigType TConfigType, typename TId, typename TArg, typename TRestrict = Configuration::NoRestrictions>
 	inline void Set(TId&& aId, TArg&& aValue, EConfigGroup aConfigGroup, TRestrict aRestrict = Configuration::NoRestrictions())
 		requires(
@@ -31,7 +38,54 @@ public:
 			CConfig<std::remove_reference_t<TArg>>
 			&& std::is_constructible_v<TConfigType, TArg>)
 	{
-		myConfiguration.SetOverride<TConfigType, TRaw>(aId, std::forward<TArg>(aValue));
+		if constexpr (TRaw)
+		{
+			const auto configs = myConfiguration.GetConfigs();
+
+			const auto it = configs.find(aId);
+
+			if (it == configs.end())
+				return;
+
+			auto& [id, element] = *it;
+			using configData = Configuration::ConfigData<TConfigType>;
+
+			if (!std::holds_alternative<configData>(element))
+				return;
+
+			auto& data = std::get<configData>(element);
+
+			if (data.Override)
+			{
+				if (*data.Override == aValue)
+					return;
+			}
+			else if (data.Value == aValue)
+				return;
+		}
+		else
+		{
+			const auto config = myConfiguration.Get<TConfigType>(aId, aValue);
+
+			if (config == aValue)
+				return;
+		}
+		
+		myConfiguration.SetOverride<TConfigType, TRaw>(aId, aValue);
+
+		BufferExecutor executor;
+		SOverrideData overrideData = { aId };
+
+		if constexpr (std::is_enum_v<TConfigType>)
+			overrideData.Value = static_cast<std::underlying_type_t<TConfigType>>(aValue);
+		else
+			overrideData.Value = std::forward<TArg>(aValue);
+
+		for (auto& subscriberData : myEventList)
+			if (!subscriberData.ConfigId || *subscriberData.ConfigId == aId)
+				subscriberData.Trigger.Trigger(executor, overrideData);
+
+		executor.ExecuteAll();
 	}
 
 	template<CConfigType TConfigType, typename TDefaultArg>
@@ -61,7 +115,66 @@ public:
 
 	inline void ResetOverride(const std::string& aId)
 	{
+		const auto configs = myConfiguration.GetConfigs();
+
+		const auto it = configs.find(aId);
+
+		if (it == configs.end())
+			return;
+
+		auto& [id, element] = *it;
+		BufferExecutor executor;
+
+		std::visit([&executor, &aId, &eventList = myEventList](const auto& aConfigData)
+			{
+				if (!aConfigData.Override)
+					return;
+				else if (*aConfigData.Override == aConfigData.Value)
+					return;
+
+				const SOverrideData overrideData = { aId, aConfigData.Value };
+
+				for (auto& subscriberData : eventList)
+					if (!subscriberData.ConfigId || *subscriberData.ConfigId == aId)
+						subscriberData.Trigger.Trigger(executor, overrideData);
+
+			}, element);
+		
 		myConfiguration.ResetOverride(aId);
+
+		executor.ExecuteAll();
+	}
+
+	template<CExecutor TExecutor, typename TCallable>
+	inline EventSubscriber OnOverrideChanged(TExecutor aExecutor, TCallable&& aCallable)
+		requires(std::is_invocable_v<const TCallable, const std::string&>)
+	{
+		auto [trigger, subscriber] = MakeEventTrigger<SOverrideData>(std::move(aExecutor), [callable = std::forward<TCallable>(aCallable)](const auto aOverrideData)
+			{
+				callable(aOverrideData.Id);
+			});
+
+		myEventList.Emplace(SOverrideSubscriberData{ std::move(trigger) });
+
+		return std::move(subscriber);
+	}
+	template<CConfig TConfig, typename TId, CExecutor TExecutor, typename TCallable>
+	inline EventSubscriber OnOverrideChanged(TId&& aId, TExecutor aExecutor, TCallable&& aCallable)
+		requires(
+			std::is_invocable_v<const TCallable, const std::string&, const TConfig&>
+			&& (std::is_constructible_v<std::string, TId> || std::is_convertible_v<TId, std::string>))
+	{
+		auto [trigger, subscriber] = MakeEventTrigger<SOverrideData>(std::move(aExecutor), [callable = std::forward<TCallable>(aCallable)](const auto aOverrideData)
+			{
+				if constexpr (std::is_enum_v<TConfig>)
+					callable(aOverrideData.Id, static_cast<TConfig>(std::get<std::underlying_type_t<TConfig>>(aOverrideData.Value)));
+				else
+					callable(aOverrideData.Id, std::get<TConfig>(aOverrideData.Value));
+			});
+
+		myEventList.Emplace(SOverrideSubscriberData{ std::move(trigger), static_cast<std::string>(std::forward<TId>(aId)) });
+
+		return std::move(subscriber);
 	}
 
 	inline [[nodiscard]] std::unordered_map<std::string, Configuration::ConfigElement> GetConfigs() const
@@ -74,6 +187,27 @@ public:
 	}
 
 private:
+	struct SOverrideData
+	{
+		using ValueType = std::variant<bool, f32, f64, u8, u16, u32, u64, i8, i16, i32, i64, std::string>;
+
+		std::string	Id;
+		ValueType	Value;
+	};
+
+	struct SOverrideSubscriberData
+	{
+		EventTrigger<SOverrideData>	Trigger;
+		std::optional<std::string>	ConfigId;
+
+		inline void OnCancel(CExecutor auto&& aExecutor, auto&& aOnCancel)
+		{
+			WmCallOnCancel(Trigger, std::forward<decltype(aExecutor)>(aExecutor), std::forward<decltype(aOnCancel)>(aOnCancel));
+		}
+	};
+
 	Configuration myConfiguration;
+
+	CancelableList<SOverrideSubscriberData> myEventList;
 
 };

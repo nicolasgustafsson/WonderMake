423e8d2cd152dc73c4edc34514e9e396b4863f84
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-10-13 17:38:30 +0200

    Binding weak_ptr now returns a result.

--- lib/wondermake-utility/include/wondermake-utility/Bindable.h
+++ lib/wondermake-utility/include/wondermake-utility/Bindable.h
@@ -1,13 +1,23 @@
 #pragma once
 
+#include "wondermake-utility/Result.h"
+
 #include <memory>
 #include <tuple>
 
-template<typename TCallable, typename... TPackedArgs>
+namespace _Impl
+{
+	struct Bind {};
+	struct WeakBind {};
+}
+
+template<typename TBinding, typename TCallable, typename... TPackedArgs>
 class Bindable
 {
 public:
-	inline constexpr Bindable(TCallable&& aCallable, TPackedArgs&&... aArgs)
+	using Binding = TBinding;
+
+	inline constexpr Bindable(const TBinding&, TCallable&& aCallable, TPackedArgs&&... aArgs)
 		: myCallable(std::forward<TCallable>(aCallable))
 		, myPackedArgs(std::forward<TPackedArgs>(aArgs)...)
 	{}
@@ -45,13 +55,13 @@ private:
 template<typename TCallable, typename... TArgs>
 auto Bind(TCallable&& aCallable, TArgs&&... aArgs)
 {
-	return Bindable(std::forward<TCallable>(aCallable), std::forward<TArgs>(aArgs)...);
+	return Bindable(::_Impl::Bind(), std::forward<TCallable>(aCallable), std::forward<TArgs>(aArgs)...);
 }
 
 template<typename TFunc, typename TSelf, typename... TArgs>
 auto Bind(TFunc aFunction, TSelf& aSelf, TArgs... aArgs) requires(std::is_member_function_pointer_v<TFunc>)
 {
-	return Bindable([aFunction, &self = aSelf](auto&&... aAllArgs)
+	return Bindable(::_Impl::Bind(), [aFunction, &self = aSelf](auto&&... aAllArgs)
 		{
 			return (self.*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...);
 		}, std::move(aArgs)...);
@@ -60,7 +70,7 @@ auto Bind(TFunc aFunction, TSelf& aSelf, TArgs... aArgs) requires(std::is_member
 template<typename TFunc, typename TSelf, typename... TArgs>
 auto Bind(TFunc aFunction, TSelf* aSelf, TArgs... aArgs) requires(std::is_member_function_pointer_v<TFunc>)
 {
-	return Bindable([aFunction, self = aSelf](auto&&... aAllArgs)
+	return Bindable(::_Impl::Bind(), [aFunction, self = aSelf](auto&&... aAllArgs)
 		{
 			return (self->*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...);
 		}, std::move(aArgs)...);
@@ -69,20 +79,44 @@ auto Bind(TFunc aFunction, TSelf* aSelf, TArgs... aArgs) requires(std::is_member
 template<typename TFunc, typename TSelf, typename... TArgs>
 auto Bind(TFunc aFunction, std::shared_ptr<TSelf> aSelf, TArgs... aArgs) requires(std::is_member_function_pointer_v<TFunc>)
 {
-	return Bindable([aFunction, self = std::move(aSelf)](auto&&... aAllArgs)
+	return Bindable(::_Impl::Bind(), [aFunction, self = std::move(aSelf)](auto&&... aAllArgs)
 	{
 		return (self.get()->*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...);
 	}, std::move(aArgs)...);
 }
 
+namespace _Impl
+{
+	struct WeakCallableError {};
+}
+
 template<typename TFunc, typename TSelf, typename... TArgs>
 auto Bind(TFunc aFunction, std::weak_ptr<TSelf> aSelf, TArgs... aArgs) requires(std::is_member_function_pointer_v<TFunc>)
 {
-	return Bindable([aFunction, self = std::move(aSelf)](auto&&... aAllArgs)
+	return Bindable(::_Impl::WeakBind(), [aFunction, self = std::move(aSelf)](auto&&... aAllArgs)
 	{
-		auto ptr = self.lock();
+		using RetType = decltype((self.lock().get()->*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...));
 
-		if (ptr)
-			(void)(ptr.get()->*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...);
+	 	static constexpr auto Invoke = [](TFunc aFunction, const std::weak_ptr<TSelf>& aSelf, auto&&... aAllArgs) -> Result<RetType, _Impl::WeakCallableError>
+		{
+			auto ptr = aSelf.lock();
+	
+			if (!ptr)
+				return Err(::_Impl::WeakCallableError {});
+	
+			if constexpr (std::is_void_v<RetType>)
+			{
+				(ptr.get()->*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...);
+
+				return Ok();
+			}
+			else
+				return Ok((ptr.get()->*aFunction)(std::forward<decltype(aAllArgs)>(aAllArgs)...));
+		};
+
+		return Invoke(aFunction, self, std::forward<decltype(aAllArgs)>(aAllArgs)...);
 	}, std::move(aArgs)...);
 }
+
+template<typename TCallable>
+concept CWeakBindedCallable = std::is_same_v<typename TCallable::Binding, ::_Impl::WeakBind>;

556465acb8ee58facbd1a0120a4a90226311bcf9
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-10-20 16:17:11 +0200

    Moved ConfigurationRemoteSystem dress rehearsal.

--- test/wondermake-engine-tests/tests/ConfigurationRemoteSystemTests.cpp
+++ test/wondermake-engine-tests/tests/ConfigurationRemoteSystemTests.cpp
@@ -1,5 +1,7 @@
 #include "wondermake-tests-common/GTestInclude.h"
 
+#include "ConfigurationRemoteSystemTests.h"
+
 #include "WonderMakeIoTests/SocketAcceptorMock.h"
 #include "WonderMakeIoTests/SocketMock.h"
 
@@ -53,30 +55,6 @@ MATCHER_P(ProtobufMessageMatcher, aExpectedProto, "")
 using ProtoConfigurationRemote::EDataType;
 using ProtoConfigurationRemote::Upstream;
 using ProtoConfigurationRemote::Downstream;
-
-auto MakeConfigurationRemoteSystem(std::optional<Guid> aInstanceId = std::nullopt) -> std::tuple<SharedReference<ConfigurationSystem>, SharedReference<NiceMock<GuidGeneratorSystemMock>>, SharedReference<ConfigurationRemoteSystem>>
-{
-	ConfigurationSystem::InjectDependencies(std::tie());
-
-	auto configSys = MakeSharedReference<ConfigurationSystem>();
-	auto guidSys = MakeSharedReference<NiceMock<GuidGeneratorSystemMock>>();
-
-	configSys->Initialize();
-	guidSys->DelegateToFake();
-
-	if (aInstanceId)
-		EXPECT_CALL(*guidSys, GenerateNew)
-		.WillOnce(Return(*aInstanceId))
-		.WillRepeatedly(DoDefault());
-
-	ConfigurationRemoteSystem::InjectDependencies(std::tie(*configSys, *guidSys));
-
-	auto configRemoteSys = MakeSharedReference<ConfigurationRemoteSystem>();
-
-	configRemoteSys->Initialize();
-
-	return { std::move(configSys), std::move(guidSys), std::move(configRemoteSys) };
-}
 
 template<typename TType>
 auto MakeSetOverride(auto& aProtoSetOverride, const Guid aInstanceId, const std::string& aId, const std::optional<TType>& aOverrideValue)
@@ -948,87 +926,4 @@ TEST(ConfigurationRemoteSystemTests, push_down_set_override_is_passed_between_pr
 	TestPushDownSetOverridePassedBetweenProcesses<i32>			(1);
 	TestPushDownSetOverridePassedBetweenProcesses<i64>			(1);
 	TestPushDownSetOverridePassedBetweenProcesses<std::string>	("override");
-}
-
-TEST(ConfigurationRemoteSystemTests, dress_rehearsal)
-{
-	static constexpr auto instanceIdA	= *Guid::Parse("38d1d3df-82ba-4f8a-a30e-510110a94a0d");
-	static constexpr auto instanceIdB	= *Guid::Parse("e8992896-f718-4d1a-97ff-06daab3558a4");
-	static constexpr auto instanceIdC	= *Guid::Parse("162d0323-9fe5-46ed-9598-849f2d1998f3");
-
-	static constexpr auto dummyId			= "dummy_id";
-	static constexpr auto dummyValue		= "value";
-	static constexpr auto dummyOverrideA	= "override_a";
-	static constexpr auto dummyOverrideB	= "override_b";
-
-	static constexpr auto findConfig = [](const Guid& aGuid)
-	{
-		return [guid = aGuid](const auto& aTuple)
-		{
-			const auto& [id, _] = aTuple;
-
-			return guid == id;
-		};
-	};
-
-	auto [configSysA, guidSysA, configRemoteSysA] = MakeConfigurationRemoteSystem(instanceIdA);
-	auto [configSysB, guidSysB, configRemoteSysB] = MakeConfigurationRemoteSystem(instanceIdB);
-	auto socketAcceptorMockA = MakeSharedReference<NiceMock<SocketAcceptorMock>>();
-	auto [socketADn, socketBUp] = MakeLinkedSockets();
-
-	const auto testConfiguration = [](const auto& aConfigRemote, const auto& aInstanceIdDn, const auto& aConfig)
-	{
-		auto begin	= aConfigRemote->begin();
-		auto end	= aConfigRemote->end();
-
-		const auto it = std::find_if(begin, end, findConfig(aInstanceIdDn));
-
-		ASSERT_NE(it, end);
-
-		const auto& [_, result] = *it;
-
-		EXPECT_EQ(result.Configuration, aConfig->GetConfiguration());
-	};
-
-	configSysB->Set<std::string>(dummyId, dummyValue, EConfigGroup::Application);
-
-	socketAcceptorMockA->DelegateToFake();
-
-	configRemoteSysA->AcceptConnectionFrom(socketAcceptorMockA);
-	configRemoteSysB->ConnectTo(std::move(socketBUp));
-
-	socketAcceptorMockA->CompleteNextConnection(Ok(std::move(socketADn)));
-
-	testConfiguration(configRemoteSysA, instanceIdB, configSysB);
-
-	configRemoteSysA->SetOverride<std::string>(instanceIdB, dummyId, dummyOverrideA);
-
-	EXPECT_EQ(configSysB->Get<std::string>(dummyId, "default"), dummyOverrideA);
-
-	configSysB->SetOverride<std::string>(dummyId, dummyOverrideB);
-
-	testConfiguration(configRemoteSysA, instanceIdB, configSysB);
-
-	{
-		auto [configSysC, guidSysC, configRemoteSysC] = MakeConfigurationRemoteSystem(instanceIdC);
-		auto socketAcceptorMockB = MakeSharedReference<NiceMock<SocketAcceptorMock>>();
-		auto [socketBDn, socketCUp] = MakeLinkedSockets();
-
-		socketAcceptorMockB->DelegateToFake();
-
-		configRemoteSysB->AcceptConnectionFrom(socketAcceptorMockB);
-		configRemoteSysC->ConnectTo(std::move(socketCUp));
-
-		socketAcceptorMockB->CompleteNextConnection(Ok(std::move(socketBDn)));
-
-		testConfiguration(configRemoteSysA, instanceIdB, configSysB);
-		testConfiguration(configRemoteSysA, instanceIdC, configSysC);
-		testConfiguration(configRemoteSysB, instanceIdC, configSysC);
-	}
-
-	auto begin	= configRemoteSysB->begin();
-	auto end	= configRemoteSysB->end();
-
-	EXPECT_EQ(begin, end);
-	testConfiguration(configRemoteSysA, instanceIdB, configSysB);
 }

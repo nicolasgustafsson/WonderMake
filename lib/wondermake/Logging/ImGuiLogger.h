40fc2e6d6885af4880f1eb005f1905470e7adcd4
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-10-21 17:58:48 +0200

    Added more options to ImGuiLogger.

--- lib/wondermake/Logging/ImGuiLogger.h
+++ lib/wondermake/Logging/ImGuiLogger.h
@@ -5,6 +5,14 @@
 #include "wondermake-base/Logger.h"
 #include "wondermake-base/System.h"
 
+#include "wondermake-utility/Guid.h"
+
+#include <optional>
+#include <string>
+#include <string_view>
+#include <set>
+#include <vector>
+
 class ImGuiLogger
 	: public Debugged
 	, public LoggerBase
@@ -22,13 +30,71 @@ public:
 	void Initialize() override;
 
 private:
+	struct SImGuiLogLine
+	{
+		SColor						Color;
+		std::string_view			Severity	= "Unset";
+		std::string_view			Level		= "Unset";
+		std::string					Message;
+		std::string					File;
+		std::string					FileWithLine;
+		std::string					Timestamp;
+		std::string					ThreadHash;
+		std::string					LoggerName;
+		std::string					ProcessId;
+		std::vector<std::string>	LogTags;
+	};
+	struct SImGuiCompactLogLine
+	{
+		SImGuiLogLine	LogLine;
+		u64				Count = 0;
+	};
+	struct SCategoryValues
+	{
+		std::set<std::string_view>	Severity;
+		std::set<std::string_view>	Level;
+		std::set<std::string>		File;
+		std::set<std::string>		ThreadHash;
+		std::set<std::string>		LoggerName;
+		std::set<std::string>		ProcessId;
+		std::set<std::string>		Tags;
+	};
+
+	struct SVisibleColumns
+	{
+		bool LoggerName	= false;
+		bool ProcessId	= false;
+		bool ThreadHash	= false;
+		bool Timestamp	= false;
+		bool Severity	= false;
+		bool Level		= false;
+		bool File		= false;
+		bool MsgCount	= false;
+		bool Message	= false;
+
+		bool operator==(const SVisibleColumns&) const = default;
+	};
+
 	void Debug() override;
+	template<bool CompactView>
+	void UpdateLogs();
+
+	void ReconstructCompactCache();
+	void AddLogLineToCompactCache(const SImGuiLogLine& aLogLine);
 
 	void Print(const SLogLine& aLogLine) override;
 
-	std::vector<SLogLine> myLogMessages;
+	static std::vector<std::string> ExtractLogTags(std::string_view aMessage);
 
-	std::string myFilterText;
+	SCategoryValues						myCategoryValues;
+	std::vector<SImGuiLogLine>			myLogMessages;
+	std::vector<SImGuiCompactLogLine>	myCompactLogLinesCache;
+	bool								myDirtyCompactCache = true;
+	SVisibleColumns						myCurrentVisibleColumns;
+
+	std::string		myFilterText;
+	SCategoryValues	myFilterCategoryValues;
+	bool			myCompactView = false;
 
 	bool myIsAtBottom = true;
 	std::optional<f32> myPreviousScrollY;

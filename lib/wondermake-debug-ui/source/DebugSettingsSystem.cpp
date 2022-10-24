ab70bac62a9882f94bccb1c8e37f545d74126f6f
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-10-24 18:28:10 +0200

    Added DebugSystem and migrated functions from DebugSettingsSystem to it.

--- lib/wondermake-debug-ui/source/DebugSettingsSystem.cpp
+++ lib/wondermake-debug-ui/source/DebugSettingsSystem.cpp
@@ -18,12 +18,8 @@
 
 REGISTER_SYSTEM(DebugSettingsSystem);
 
-inline constexpr auto locDebugSettingsWindow = "Debug Windows/Debug Settings";
-
 DebugSettingsSystem::DebugSettingsSystem()
 {
-	AddDebugWindowTick("Debug Settings", Bind(&DebugSettingsSystem::Tick, this));
-
 	auto filePath = Get<ConfigurationSystem>().Get<FilePath>(ConfigurationDebugUi::SettingsFile, FilePath(FilePath::EFolder::Bin, "debugSettings.json"));
 
 	Get<JobSystem>()
@@ -43,31 +39,6 @@ DebugSettingsSystem::DebugSettingsSystem()
 					mySettings = json::parse(json);
 			}))
 		.Detach();
-}
-
-void DebugSettingsSystem::ToggleSettingsWindow()
-{
-	const bool isVisible = GetOrCreateDebugValue<bool>(locDebugSettingsWindow, false);
-
-	SetDebugValue<bool>(locDebugSettingsWindow, !isVisible);
-
-	SaveSettings();
-}
-
-void DebugSettingsSystem::TickAllWindows()
-{
-	for (auto& [name, callback] : myTickCallbacks)
-	{
-		if (!GetOrCreateDebugValue<bool>(name, false))
-			continue;
-
-		Utility::Invoke(callback);
-	}
-}
-
-void DebugSettingsSystem::AddDebugWindowTick(const std::string& aWindowName, std::function<void()> aTickCallback)
-{
-	myTickCallbacks.insert(std::make_pair("Debug Windows/" + aWindowName, std::move(aTickCallback)));
 }
 
 void DebugSettingsSystem::SaveSettings()
@@ -98,12 +69,12 @@ void DebugSettingsSystem::Tick()
 		SaveSettings();
 }
 
-std::pair<nlohmann::json&, std::string> DebugSettingsSystem::GetLeaf(const std::string aSettingName)
+std::pair<nlohmann::json&, std::string> DebugSettingsSystem::GetLeaf(const std::string& aSettingName)
 {
 	nlohmann::json* currentJson = &mySettings;
 
-	size_t last = 0; 
-	size_t next = 0; 
+	size_t last = 0;
+	size_t next = 0;
 	while ((next = aSettingName.find('/', last)) != std::string::npos)
 	{ 
 		std::string category = aSettingName.substr(last, next - last);

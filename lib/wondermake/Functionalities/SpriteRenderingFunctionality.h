a810b033cabbc5a927cf64437edb30250ada1ec3
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-11-07 14:22:41 +0100

    Sprites can now use FilePath.

--- lib/wondermake/Functionalities/SpriteRenderingFunctionality.h
+++ lib/wondermake/Functionalities/SpriteRenderingFunctionality.h
@@ -4,10 +4,19 @@
 #include "Components/SpriteComponent.h"
 #include "Functionalities/TransformFunctionality.h"
 
+#include "wondermake-utility/FilePath.h"
+
+template<typename TResource>
+class ResourceProxy;
+template<typename TResource>
+class ResourceSystem;
+class Texture;
+
 class SpriteRenderingFunctionality
 	: public Functionality<
 		Policy::Set<
 			PAdd<AssetDatabase<Texture>, PWrite>,
+			PAdd<ResourceSystem<Texture>, PWrite>,
 			PAdd<TransformFunctionality2D, PRead>,
 			PAdd<SSpriteComponent, PWrite>>,
 		SystemTraits::Set<
@@ -15,11 +24,19 @@ class SpriteRenderingFunctionality
 {
 public:
 	void Tick();
+
 	void SetTexture(std::string_view aAssetLink);
+	void SetTexture(const FilePath& aFilePath);
+
 	void SetScale(const SVector2f aScale);
 	void SetRotation(const f32 aRotation);
 	void SetOrigin(const SVector2f aOrigin);
 	void SetColor(const SColor aColor);
+
 	void Hide() noexcept;
 	void Show() noexcept;
+
+private:
+	void SetTexture(ResourceProxy<Texture>&& aTexture);
+
 };

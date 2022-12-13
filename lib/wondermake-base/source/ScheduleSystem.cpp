1892f1136cf9e56f7554f6c1996e0f9611e6e7d6
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-12-13 11:04:54 +0100

    Added a singleton version of ScheduleSystem.

--- lib/wondermake-base/source/ScheduleSystem.cpp
+++
@@ -1,6 +0,0 @@
-#include "wondermake-base/ScheduleSystem.h"
-
-ScheduleSystem::ScheduleSystem(ScheduleProc aScheduleProc, ScheduleRepeatingProc aScheduleRepeatingProc) noexcept
-	: myScheduleProc(std::move(aScheduleProc))
-	, myScheduleRepeatingProc(std::move(aScheduleRepeatingProc))
-{}

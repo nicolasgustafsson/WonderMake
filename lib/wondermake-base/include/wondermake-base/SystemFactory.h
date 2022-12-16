08b7b25ce8d830280ce8bdf082dc14116b2fc74b
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2022-12-16 16:13:37 +0100

    Added SystemFactory.

---
+++ lib/wondermake-base/include/wondermake-base/SystemFactory.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "wondermake-base/System.h"
+#include "wondermake-base/SystemContainer.h"
+#include "wondermake-base/SystemRegistry.h"
+
+#include "wondermake-utility/AnyExecutor.h"
+#include "wondermake-utility/EventSubscriber.h"
+#include "wondermake-utility/Future.h"
+#include "wondermake-utility/Result.h"
+#include "wondermake-utility/UniqueFunction.h"
+
+#include <functional>
+#include <memory>
+
+class ScheduleSystemSingleton;
+
+class SystemFactory
+	: public System<
+		Policy::Set<
+			PAdd<ScheduleSystemSingleton, PWrite>>,
+		STrait::Set<
+			STSingleton,
+			STWonderMake>>
+{
+public:
+	Future<Result<std::shared_ptr<SystemContainer>, SystemRegistry::SError>> CreateSystems(
+		SystemRegistry::Filter																	aFilter,
+		std::function<Future<void>(AnyExecutor aExecutor, Closure&& aTask)>						aScheduleFunc			= nullptr,
+		std::function<EventSubscriber(AnyExecutor aExecutor, std::function<void()>&& aTask)>	aScheduleRepeatingFunc	= nullptr);
+};

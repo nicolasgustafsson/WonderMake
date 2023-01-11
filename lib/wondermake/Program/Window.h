bd73bed8acc32cd92f9c9eafb11a957c0aa98709
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2023-01-11 17:58:41 +0100

    Added on scroll subscription.

--- lib/wondermake/Program/Window.h
+++ lib/wondermake/Program/Window.h
@@ -55,11 +55,22 @@ public:
 
 		return std::move(subscriber);
 	}
+	template<CExecutor TExecutor, typename TCallable>
+	inline EventSubscriber OnScroll(TExecutor&& aExecutor, TCallable&& aCallable)
+		requires(std::is_invocable_v<const TCallable, SVector2f>)
+	{
+		auto [trigger, subscriber] = MakeEventTrigger<SVector2f>(std::forward<TExecutor>(aExecutor), std::forward<TCallable>(aCallable));
+
+		myScrollList.Emplace(std::move(trigger));
+
+		return std::move(subscriber);
+	}
 
 	GLFWwindow* myGlfwWindow = nullptr;
 
 private:
 	void OnWindowSizeChanged(GLFWwindow* aWindow, i32 aWidth, i32 aHeight);
+	void OnScroll(GLFWwindow* aWindow, double aDeltaX, double aDeltaY);
 	void UpdatePosition();
 
 	EventSubscriber mySubscriberWindowTitle;
@@ -67,6 +78,7 @@ private:
 	EventSubscriber mySubscriberSizeHeight;
 
 	CancelableList<EventTrigger<SVector2i>> myResizeList = { GetExecutor() };
+	CancelableList<EventTrigger<SVector2f>> myScrollList = { GetExecutor() };
 
 	SVector2i mySize;
 	SVector2i myPosition;

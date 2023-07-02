6ab33e5f464fbf69f126f7e46e8482f722f3e047
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2023-07-02 21:39:37 +0200

    Add config for extra bin folders

--- lib/wondermake-io/source/ConfigurationIo.cpp
+++ lib/wondermake-io/source/ConfigurationIo.cpp
@@ -10,8 +10,9 @@ namespace ConfigurationIo
 		std::filesystem::path	aPathUser,
 		std::filesystem::path	aPathUserData)
 	{
-		aConfigurationSystem.Set<std::string>(static_cast<std::string>(ConfigDirectoryData),		std::move(aPathData).string(),		EConfigGroup::Application);
-		aConfigurationSystem.Set<std::string>(static_cast<std::string>(ConfigDirectoryUser),		std::move(aPathUser).string(),		EConfigGroup::Application);
-		aConfigurationSystem.Set<std::string>(static_cast<std::string>(ConfigDirectoryUserData),	std::move(aPathUserData).string(),	EConfigGroup::Application);
+		aConfigurationSystem.Set<std::string>(ConfigDirectoryExtraBins,	"",									EConfigGroup::Application);
+		aConfigurationSystem.Set<std::string>(ConfigDirectoryData,		std::move(aPathData).string(),		EConfigGroup::Application);
+		aConfigurationSystem.Set<std::string>(ConfigDirectoryUser,		std::move(aPathUser).string(),		EConfigGroup::Application);
+		aConfigurationSystem.Set<std::string>(ConfigDirectoryUserData,	std::move(aPathUserData).string(),	EConfigGroup::Application);
 	}
 }

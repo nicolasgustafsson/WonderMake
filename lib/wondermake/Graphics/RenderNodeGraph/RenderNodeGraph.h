5ec8302fbefb8472955d967ca0999a8c37c95bb4
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2023-07-03 00:07:48 +0200

    Make Node Graphs use the first file it finds

--- lib/wondermake/Graphics/RenderNodeGraph/RenderNodeGraph.h
+++ lib/wondermake/Graphics/RenderNodeGraph/RenderNodeGraph.h
@@ -1,13 +1,15 @@
 #pragma once
 #include "NodeGraph/NodeGraph.h"
 
+#include "wondermake-utility/FilePath.h"
+
 class RenderTarget;
 
 class RenderNodeGraph
 	: public NodeGraph
 {
 public:
-	RenderNodeGraph(std::filesystem::path aPath);
+	RenderNodeGraph(FilePath aPath);
 
 	RenderTarget* GetFinalRenderTarget() const;
 protected:

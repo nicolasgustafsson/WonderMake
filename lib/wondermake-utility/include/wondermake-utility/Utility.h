7843050f0e1e6c66d657f831136dacbccb4f17f7
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2023-09-15 15:22:08 +0200

    Add GetTypeName to Utility

--- lib/wondermake-utility/include/wondermake-utility/Utility.h
+++ lib/wondermake-utility/include/wondermake-utility/Utility.h
@@ -5,6 +5,24 @@
 
 namespace Utility
 {
+	template<typename T>
+	[[nodiscard]] inline std::string_view GetTypeName() noexcept
+	{
+		static constexpr std::string_view prefixClass	= "class ";
+		static constexpr std::string_view prefixStruct	= "struct ";
+
+		// std::type_info::name makes no guarantees of the format
+		std::string_view typeName = typeid(T).name();
+
+		if (typeName.starts_with(prefixClass))
+			return typeName.substr(prefixClass.size());
+
+		if (typeName.starts_with(prefixStruct))
+			return typeName.substr(prefixStruct.size());
+
+		return typeName;
+	}
+
 	template<typename TCallable, typename... TArgs> requires std::is_invocable_v<TCallable, TArgs...>
 	static void Invoke(const TCallable& aCallable, TArgs&&... aArgs)
 	{

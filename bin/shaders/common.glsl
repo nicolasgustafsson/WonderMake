190ebe88ce88724eab6cf890824394a66eab16a5
Kevin Zachrisson <kevin.zachrisson@hotmail.com>
2023-10-04 18:26:36 +0200

    Add common functions to common.glsl

--- bin/shaders/common.glsl
+++ bin/shaders/common.glsl
@@ -9,3 +9,36 @@ vec2 CorrectSubpixelPositioning(vec2 aPosition)
 	
 	return aPosition * InverseResolution;
 }
+
+mat3 CreateRotationAroundX(float aRadians)
+{
+	return mat3(
+			1, 0, 0,
+			0, cos(aRadians), -sin(aRadians),
+			0, sin(aRadians), cos(aRadians));
+}
+mat3 CreateRotationAroundY(float aRadians)
+{
+	return mat3(
+			cos(aRadians), 0, sin(aRadians),
+			0, 1, 0,
+			-sin(aRadians), 0, cos(aRadians));
+}
+mat3 CreateRotationAroundZ(float aRadians)
+{
+	return mat3(
+			cos(aRadians), -sin(aRadians), 0,
+			sin(aRadians), cos(aRadians), 0, 
+			0, 0, 1);
+}
+mat2 CreateRotation2D(float aRadians)
+{
+	return mat2(
+		cos(aRadians), -sin(aRadians),
+		sin(aRadians), cos(aRadians));
+}
+
+float Gaussian(float x, float mean, float stddev)
+{
+    return exp(-((x - mean) * (x - mean)) / (2.0 * stddev * stddev));
+}
